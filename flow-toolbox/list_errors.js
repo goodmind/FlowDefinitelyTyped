333ce3a4273848326bfc43827eb4e7aa9a9d7441
andretshurotshka <andwebar@gmail.com>
2019-02-05 03:42:10 +0500

    Add flow-toolbox source

---
+++ flow-toolbox/list_errors.js
@@ -0,0 +1,16 @@
+//@flow
+
+const fs = require("fs-extra");
+const _ = require("lodash");
+
+async function main() {
+    const problems = await fs.readdir("./flow-types/logs");
+    for (const type of problems) {
+        try {
+            const err = await fs.readFile(`./flow-types/logs/${type}/err.log`);
+            console.log(type);
+        } catch (err) {}
+    }
+}
+
+main();
