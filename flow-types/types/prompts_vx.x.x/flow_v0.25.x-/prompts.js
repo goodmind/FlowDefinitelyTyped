c2ae8883fdb04181fe5404f3b0c8032ca3d71342
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:53:10 +0500

    [prompts] update types

--- flow-types/types/prompts_vx.x.x/flow_v0.25.x-/prompts.js
+++ flow-types/types/prompts_vx.x.x/flow_v0.25.x-/prompts.js
@@ -1,109 +1,108 @@
 declare module "prompts" {
-  declare module.exports: typeof prompts;
+  declare export default typeof prompts;
 
   declare function prompts<T: string>(
-    questions:
-      | prompts$prompts$PromptObject<T>
-      | core$Array<prompts$prompts$PromptObject<T>>,
-    notification$options?: prompts$prompts$Options
-  ): OfficeExtension$Promise<prompts$prompts$Answers<T>>;
+    questions: prompts$PromptObject<T> | Array<prompts$PromptObject<T>>,
+    options?: prompts$Options
+  ): Promise<prompts$Answers<T>>;
 
   declare var npm$namespace$prompts: {
     inject: typeof prompts$inject,
-    prompt: typeof prompts$prompt
+    prompt: typeof prompts$prompt,
+
+    inject: typeof npm$namespace$prompts$inject,
+    prompts: typeof npm$namespace$prompts$prompts
   };
   declare var prompts$prompt: any;
 
   declare function prompts$inject(obj: any): void;
 
-  declare var npm$namespace$inject: {
-    prototype: typeof inject$prototype
+  declare var npm$namespace$prompts$inject: {
+    prototype: typeof prompts$inject$prototype
   };
-  declare var inject$prototype: {};
+  declare var prompts$inject$prototype: {};
 
-  declare var npm$namespace$prompts: {
-    autocomplete: typeof prompts$autocomplete,
-    confirm: typeof prompts$confirm,
-    invisible: typeof prompts$invisible,
-    list: typeof prompts$list,
-    multiselect: typeof prompts$multiselect,
-    number: typeof prompts$number,
-    password: typeof prompts$password,
-    select: typeof prompts$select,
-    text: typeof prompts$text,
-    toggle: typeof prompts$toggle
+  declare var npm$namespace$prompts$prompts: {
+    autocomplete: typeof prompts$prompts$autocomplete,
+    confirm: typeof prompts$prompts$confirm,
+    invisible: typeof prompts$prompts$invisible,
+    list: typeof prompts$prompts$list,
+    multiselect: typeof prompts$prompts$multiselect,
+    number: typeof prompts$prompts$number,
+    password: typeof prompts$prompts$password,
+    select: typeof prompts$prompts$select,
+    text: typeof prompts$prompts$text,
+    toggle: typeof prompts$prompts$toggle
   };
-  declare function prompts$autocomplete(args: prompts$PromptObject): any;
+  declare function prompts$prompts$autocomplete(
+    args: prompts$PromptObject
+  ): any;
 
-  declare function prompts$confirm(args: prompts$PromptObject): void;
+  declare function prompts$prompts$confirm(args: prompts$PromptObject): void;
 
-  declare function prompts$invisible(args: prompts$PromptObject): any;
+  declare function prompts$prompts$invisible(args: prompts$PromptObject): any;
 
-  declare function prompts$list(args: prompts$PromptObject): any;
+  declare function prompts$prompts$list(args: prompts$PromptObject): any;
 
-  declare function prompts$multiselect(args: prompts$PromptObject): any;
+  declare function prompts$prompts$multiselect(args: prompts$PromptObject): any;
 
-  declare function prompts$number(args: prompts$PromptObject): void;
+  declare function prompts$prompts$number(args: prompts$PromptObject): void;
 
-  declare function prompts$password(args: prompts$PromptObject): any;
+  declare function prompts$prompts$password(args: prompts$PromptObject): any;
 
-  declare function prompts$select(args: prompts$PromptObject): void;
+  declare function prompts$prompts$select(args: prompts$PromptObject): void;
 
-  declare function prompts$text(args: prompts$PromptObject): void;
+  declare function prompts$prompts$text(args: prompts$PromptObject): void;
 
-  declare function prompts$toggle(args: prompts$PromptObject): void;
+  declare function prompts$prompts$toggle(args: prompts$PromptObject): void;
 
   declare interface prompts$Choice {
     title: string;
     value: string;
-    Minilog$disable?: boolean;
+    disable?: boolean;
   }
 
   declare interface prompts$Options {
     onSubmit?: (
-      prompts$prompt: prompts$PromptObject,
+      prompt: prompts$PromptObject,
       answer: any,
       answers: any[]
     ) => void;
-    onCancel?: (prompts$prompt: prompts$PromptObject, answers: any) => void;
+    onCancel?: (prompt: prompts$PromptObject, answers: any) => void;
   }
 
   declare interface prompts$PromptObject<T: string = string> {
-    notification$type: prompts$ValueOrFunc<prompts$PromptType> | prompts$Falsy;
-    skin$name: prompts$ValueOrFunc<T>;
+    type: prompts$ValueOrFunc<prompts$PromptType> | prompts$Falsy;
+    name: prompts$ValueOrFunc<T>;
     message?: prompts$ValueOrFunc<string>;
-    PreludeLS$initial?: string | number | boolean;
-    simplestyle$style?: string;
-    Util$format?: prompts$PrevCaller<T, void>;
+    initial?: string | number | boolean;
+    style?: string;
+    format?: prompts$PrevCaller<T, void>;
     validate?: prompts$PrevCaller<T, void>;
     onState?: prompts$PrevCaller<T, void>;
-    PreludeLS$min?: number;
-    PreludeLS$max?: number;
-    getenv$float?: boolean;
-    PreludeLS$round?: number;
+    min?: number;
+    max?: number;
+    float?: boolean;
+    round?: number;
     increment?: number;
     seperator?: string;
     active?: string;
     inactive?: string;
     choices?: prompts$Choice[];
     hint?: string;
-    Minilog$suggest?: (
-      prev: any,
-      PreludeLS$values: any,
-      prompts$prompt: prompts$PromptObject
-    ) => void;
+    suggest?: (prev: any, values: any, prompt: prompts$PromptObject) => void;
     limit?: number;
   }
 
   declare type prompts$Answers<T: string> = $ObjMapi<
     { [k: T]: any },
-    <PreludeLS$id>(PreludeLS$id) => any
+    <id>(id) => any
   >;
 
   declare type prompts$PrevCaller<T: string, R = T> = (
     prev: any,
-    PreludeLS$values: prompts$Answers<T>,
-    prompts$prompt: prompts$PromptObject
+    values: prompts$Answers<T>,
+    prompt: prompts$PromptObject
   ) => R;
 
   declare type prompts$Falsy = false | null | void;
