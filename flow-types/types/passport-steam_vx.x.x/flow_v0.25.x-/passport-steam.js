65a599f224ec79950f44cd8e9f2370a33cc93576
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:51:03 +0500

    [passport-steam] update types

--- flow-types/types/passport-steam_vx.x.x/flow_v0.25.x-/passport-steam.js
+++ flow-types/types/passport-steam_vx.x.x/flow_v0.25.x-/passport-steam.js
@@ -1,71 +1,85 @@
 declare module "passport-steam" {
-  declare module.exports: typeof passport_steam;
+  declare export default typeof passport_steam;
 
   declare function passport_steam(options: any, validate: any): any;
 
   declare var npm$namespace$passport_steam: {
     Strategy: typeof passport_steam$Strategy,
-    version: typeof passport_steam$version
+    version: typeof passport_steam$version,
+
+    prototype: typeof npm$namespace$passport_steam$prototype
   };
   declare var passport_steam$Strategy: any;
 
   declare var passport_steam$version: string;
 
-  declare var npm$namespace$prototype: {
-    authenticate: typeof prototype$authenticate,
-    loadAssociation: typeof prototype$loadAssociation,
-    loadDiscoveredInfo: typeof prototype$loadDiscoveredInfo,
-    loadDiscoveredInformation: typeof prototype$loadDiscoveredInformation,
-    saveAssociation: typeof prototype$saveAssociation,
-    saveDiscoveredInfo: typeof prototype$saveDiscoveredInfo,
-    saveDiscoveredInformation: typeof prototype$saveDiscoveredInformation
+  declare var npm$namespace$passport_steam$prototype: {
+    authenticate: typeof passport_steam$prototype$authenticate,
+    loadAssociation: typeof passport_steam$prototype$loadAssociation,
+    loadDiscoveredInfo: typeof passport_steam$prototype$loadDiscoveredInfo,
+    loadDiscoveredInformation: typeof passport_steam$prototype$loadDiscoveredInformation,
+    saveAssociation: typeof passport_steam$prototype$saveAssociation,
+    saveDiscoveredInfo: typeof passport_steam$prototype$saveDiscoveredInfo,
+    saveDiscoveredInformation: typeof passport_steam$prototype$saveDiscoveredInformation,
+
+    authenticate: typeof npm$namespace$passport_steam$prototype$authenticate,
+    loadAssociation: typeof npm$namespace$passport_steam$prototype$loadAssociation,
+    loadDiscoveredInfo: typeof npm$namespace$passport_steam$prototype$loadDiscoveredInfo,
+    loadDiscoveredInformation: typeof npm$namespace$passport_steam$prototype$loadDiscoveredInformation,
+    saveAssociation: typeof npm$namespace$passport_steam$prototype$saveAssociation,
+    saveDiscoveredInfo: typeof npm$namespace$passport_steam$prototype$saveDiscoveredInfo,
+    saveDiscoveredInformation: typeof npm$namespace$passport_steam$prototype$saveDiscoveredInformation
   };
-  declare function prototype$authenticate(req: any): any;
+  declare function passport_steam$prototype$authenticate(req: any): any;
 
-  declare function prototype$loadAssociation(fn: any): any;
+  declare function passport_steam$prototype$loadAssociation(fn: any): any;
 
-  declare function prototype$loadDiscoveredInfo(fn: any): any;
+  declare function passport_steam$prototype$loadDiscoveredInfo(fn: any): any;
 
-  declare function prototype$loadDiscoveredInformation(fn: any): any;
+  declare function passport_steam$prototype$loadDiscoveredInformation(
+    fn: any
+  ): any;
 
-  declare function prototype$saveAssociation(fn: any): any;
+  declare function passport_steam$prototype$saveAssociation(fn: any): any;
 
-  declare function prototype$saveDiscoveredInfo(fn: any): any;
+  declare function passport_steam$prototype$saveDiscoveredInfo(fn: any): any;
 
-  declare function prototype$saveDiscoveredInformation(fn: any): any;
+  declare function passport_steam$prototype$saveDiscoveredInformation(
+    fn: any
+  ): any;
 
-  declare var npm$namespace$authenticate: {
-    prototype: typeof authenticate$prototype
+  declare var npm$namespace$passport_steam$prototype$authenticate: {
+    prototype: typeof passport_steam$prototype$authenticate$prototype
   };
-  declare var authenticate$prototype: {};
+  declare var passport_steam$prototype$authenticate$prototype: {};
 
-  declare var npm$namespace$loadAssociation: {
-    prototype: typeof loadAssociation$prototype
+  declare var npm$namespace$passport_steam$prototype$loadAssociation: {
+    prototype: typeof passport_steam$prototype$loadAssociation$prototype
   };
-  declare var loadAssociation$prototype: {};
+  declare var passport_steam$prototype$loadAssociation$prototype: {};
 
-  declare var npm$namespace$loadDiscoveredInfo: {
-    prototype: typeof loadDiscoveredInfo$prototype
+  declare var npm$namespace$passport_steam$prototype$loadDiscoveredInfo: {
+    prototype: typeof passport_steam$prototype$loadDiscoveredInfo$prototype
   };
-  declare var loadDiscoveredInfo$prototype: {};
+  declare var passport_steam$prototype$loadDiscoveredInfo$prototype: {};
 
-  declare var npm$namespace$loadDiscoveredInformation: {
-    prototype: typeof loadDiscoveredInformation$prototype
+  declare var npm$namespace$passport_steam$prototype$loadDiscoveredInformation: {
+    prototype: typeof passport_steam$prototype$loadDiscoveredInformation$prototype
   };
-  declare var loadDiscoveredInformation$prototype: {};
+  declare var passport_steam$prototype$loadDiscoveredInformation$prototype: {};
 
-  declare var npm$namespace$saveAssociation: {
-    prototype: typeof saveAssociation$prototype
+  declare var npm$namespace$passport_steam$prototype$saveAssociation: {
+    prototype: typeof passport_steam$prototype$saveAssociation$prototype
   };
-  declare var saveAssociation$prototype: {};
+  declare var passport_steam$prototype$saveAssociation$prototype: {};
 
-  declare var npm$namespace$saveDiscoveredInfo: {
-    prototype: typeof saveDiscoveredInfo$prototype
+  declare var npm$namespace$passport_steam$prototype$saveDiscoveredInfo: {
+    prototype: typeof passport_steam$prototype$saveDiscoveredInfo$prototype
   };
-  declare var saveDiscoveredInfo$prototype: {};
+  declare var passport_steam$prototype$saveDiscoveredInfo$prototype: {};
 
-  declare var npm$namespace$saveDiscoveredInformation: {
-    prototype: typeof saveDiscoveredInformation$prototype
+  declare var npm$namespace$passport_steam$prototype$saveDiscoveredInformation: {
+    prototype: typeof passport_steam$prototype$saveDiscoveredInformation$prototype
   };
-  declare var saveDiscoveredInformation$prototype: {};
+  declare var passport_steam$prototype$saveDiscoveredInformation$prototype: {};
 }
