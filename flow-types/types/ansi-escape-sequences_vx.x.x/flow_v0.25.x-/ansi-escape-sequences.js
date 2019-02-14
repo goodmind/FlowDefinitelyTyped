a98cd89671a140fba368fff40049d41a57a5e566
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:07:34 +0500

    [ansi-escape-sequences] update types

--- flow-types/types/ansi-escape-sequences_vx.x.x/flow_v0.25.x-/ansi-escape-sequences.js
+++ flow-types/types/ansi-escape-sequences_vx.x.x/flow_v0.25.x-/ansi-escape-sequences.js
@@ -40,35 +40,35 @@ declare module "ansi-escape-sequences" {
   declare export var style: $ObjMapi<{ [k: Style]: any }, <K>(K) => string>;
 
   /**
- * Returns an ansi sequence setting one or more effects.
- * @param styles a style, or list or styles
- * @example > ansi.styles('green')
-'\u001b[32m'
-
-> ansi.styles([ 'green', 'underline' ])
-'\u001b[32;4m'
- */
+   * Returns an ansi sequence setting one or more effects.
+   * @param styles a style, or list or styles
+   * @example > ansi.styles('green')
+   * '\u001b[32m'
+   *
+   * > ansi.styles([ 'green', 'underline' ])
+   * '\u001b[32;4m'
+   */
   declare export function styles(styles: Style | $ReadOnlyArray<Style>): string;
 
   /**
- * A convenience function, applying the provided styles to the input string and
- * then resetting.
- * 
- * Inline styling can be applied using the syntax `[style-list]{text to
- *  * format}`, where `style-list` is a space-separated list of styles from {@link
- * module:ansi-escape-sequences.style ansi.style}. For example `[bold white
- *  * bg-red]{bold white text on a red background}`.
- * @param str the string to format
- * @param styles a style or list of styles to add to the input string
- * @example > ansi.format('what?', 'green')
-'\u001b[32mwhat?\u001b[0m'
-
-> ansi.format('what?', ['green', 'bold'])
-'\u001b[32;1mwhat?\u001b[0m'
-
-> ansi.format('[green bold]{what?}')
-'\u001b[32;1mwhat?\u001b[0m'
- */
+   * A convenience function, applying the provided styles to the input string and
+   * then resetting.
+   *
+   * Inline styling can be applied using the syntax `[style-list]{text to
+   *  * format}`, where `style-list` is a space-separated list of styles from {@link
+   * module:ansi-escape-sequences.style ansi.style}. For example `[bold white
+   *  * bg-red]{bold white text on a red background}`.
+   * @param str the string to format
+   * @param styles a style or list of styles to add to the input string
+   * @example > ansi.format('what?', 'green')
+   * '\u001b[32mwhat?\u001b[0m'
+   *
+   * > ansi.format('what?', ['green', 'bold'])
+   * '\u001b[32;1mwhat?\u001b[0m'
+   *
+   * > ansi.format('[green bold]{what?}')
+   * '\u001b[32;1mwhat?\u001b[0m'
+   */
   declare export function format(
     str: string,
     styles?: Style | $ReadOnlyArray<Style>
