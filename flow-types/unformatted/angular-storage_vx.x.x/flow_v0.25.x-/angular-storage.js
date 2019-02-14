52b7329402be833d969315fd64297b2f7140afca
andretshurotshka <andwebar@gmail.com>
2019-02-14 13:12:42 +0500

    [angular-storage] update types

--- flow-types/unformatted/angular-storage_vx.x.x/flow_v0.25.x-/angular-storage.js
+++ flow-types/unformatted/angular-storage_vx.x.x/flow_v0.25.x-/angular-storage.js
@@ -1,57 +1,48 @@
 declare module 'angular' {
-        declare type storage$IStoreService = {
+        declare type a0$storage$IStoreService = {
 
 /**
  * Returns a namespaced store
- * @param {String} namespace
- * - The namespace
- * @param {String} storage
- * - The name of the storage service. Defaults to local storage.
- * @param {String} delimiter
- * - The delimiter to use to separate the namespace and the keys.
+ * @param {String} namespace - The namespace
+ * @param {String} storage - The name of the storage service. Defaults to local storage.
+ * @param {String} delimiter - The delimiter to use to separate the namespace and the keys.
  * @returns {storage$INamespacedStoreService}
  */
 getNamespacedStore(
 namespace: string,
 storage?: string,
 delimiter?: string): storage$INamespacedStoreService
-} & storage$INamespacedStoreService
+} & a0$storage$INamespacedStoreService
 
 
-declare interface storage$INamespacedStoreService {
+declare interface a0$storage$INamespacedStoreService {
 
 /**
  * Sets a new value to the storage with the key name. It can be any object.
- * @param {String} name
- * - The key name for the location of the value
- * @param value
- * - The value to store
+ * @param {String} name - The key name for the location of the value
+ * @param value - The value to store
  */
 set(name: string, value: any): void,
 
 /**
  * Returns the saved value with they key name.
- * @param {String} name
- * - The key name for the location of the value
- * @returns
- * The saved value; if you saved an object, you get an object
+ * @param {String} name - The key name for the location of the value
+ * @returns The saved value; if you saved an object, you get an object
  */
 get(name: string): any,
 
 /**
  * Deletes the saved value with the key name
- * @param {String} name
- * - The key name for the location of the value to remove
+ * @param {String} name - The key name for the location of the value to remove
  */
 remove(name: string): void
 } 
 
-declare interface storage$IStoreProvider {
+declare interface a0$storage$IStoreProvider {
 
 /**
  * Sets the storage.
- * @param {String} storage
- * - The storage name
+ * @param {String} storage - The storage name
  */
 setStore(storage: string): void
 } 
