267bd087fe4f5de10858078148f24f7bf8ff6927
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:27:13 +0500

    [geoip-lite] update types

--- flow-types/types/geoip-lite_vx.x.x/flow_v0.25.x-/geoip-lite.js
+++ flow-types/types/geoip-lite_vx.x.x/flow_v0.25.x-/geoip-lite.js
@@ -1,24 +1,25 @@
-declare var npm$namespace$mod: {
-  lookup: typeof mod$lookup,
-  pretty: typeof mod$pretty,
-  startWatchingDataUpdate: typeof mod$startWatchingDataUpdate,
-  stopWatchingDataUpdate: typeof mod$stopWatchingDataUpdate
-};
-export interface mod$Lookup {
-  range: Array<number>;
-  country: string;
-  region: string;
-  city: string;
-  ll: Array<number>;
-}
-
-declare export function mod$lookup(ip: string): mod$Lookup;
-
-declare export function mod$pretty(ip: number): string;
-
-declare export function mod$startWatchingDataUpdate(): void;
-
-declare export function mod$stopWatchingDataUpdate(): void;
 declare module "geoip-lite" {
+  declare var npm$namespace$mod: {
+    lookup: typeof mod$lookup,
+    pretty: typeof mod$pretty,
+    startWatchingDataUpdate: typeof mod$startWatchingDataUpdate,
+    stopWatchingDataUpdate: typeof mod$stopWatchingDataUpdate
+  };
+  declare export interface mod$Lookup {
+    range: Array<number>;
+    country: string;
+    region: string;
+    city: string;
+    ll: Array<number>;
+  }
+
+  declare export function mod$lookup(ip: string): mod$Lookup;
+
+  declare export function mod$pretty(ip: number): string;
+
+  declare export function mod$startWatchingDataUpdate(): void;
+
+  declare export function mod$stopWatchingDataUpdate(): void;
+
   declare export default typeof mod;
 }
