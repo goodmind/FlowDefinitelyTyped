72ffbb0ea3b242071531fc18b8b2515a7fc74025
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:49:04 +0500

    [object-refs] update types

--- flow-types/types/object-refs_vx.x.x/flow_v0.25.x-/object-refs.js
+++ flow-types/types/object-refs_vx.x.x/flow_v0.25.x-/object-refs.js
@@ -1,63 +1,58 @@
 declare module "object-refs" {
-  declare module.exports: typeof Refs;
+  declare export default typeof Refs;
 
   declare class Refs {
     /**
      * Creates a new references object defining two inversly related
      * attribute descriptors a and b.
-     * @param {Refs$Refs$AttributeDescriptor} a property descriptor
-     * @param {Refs$Refs$AttributeDescriptor} b property descriptor
+     * @param {Refs$AttributeDescriptor} a property descriptor
+     * @param {Refs$AttributeDescriptor} b property descriptor
      */
-    constructor(
-      a: Refs$Refs$AttributeDescriptor,
-      b: Refs$Refs$AttributeDescriptor
-    ): this;
+    constructor(a: Refs$AttributeDescriptor, b: Refs$AttributeDescriptor): this;
 
     /**
      * Binds one side of a bi-directional reference to a target object.
      * @param {"NO PRINT IMPLEMENTED: JSDocAllType"} target
-     * @param {string | Refs$Refs$AttributeDescriptor} property
+     * @param {string | Refs$AttributeDescriptor} property
      */
-    bind(target: any, property: string | Refs$Refs$AttributeDescriptor): void;
-    ensureBound(
-      target: any,
-      property: string | Refs$Refs$AttributeDescriptor
-    ): void;
-    ensureRefsCollection(
-      target: any,
-      property: Refs$Refs$AttributeDescriptor
-    ): any;
+    bind(target: any, property: string | Refs$AttributeDescriptor): void;
+    ensureBound(target: any, property: string | Refs$AttributeDescriptor): void;
+    ensureRefsCollection(target: any, property: Refs$AttributeDescriptor): any;
     set(
       target: any,
-      property: string | Refs$Refs$AttributeDescriptor,
+      property: string | Refs$AttributeDescriptor,
       value: any
     ): void;
     unset(
       target: any,
-      property: string | Refs$Refs$AttributeDescriptor,
+      property: string | Refs$AttributeDescriptor,
       value: any
     ): void;
   }
+
+  declare var npm$namespace$Refs: {
+    Collection: typeof npm$namespace$Refs$Collection
+  };
   declare interface Refs$AttributeDescriptor {
     name: string;
     collection?: boolean;
     enumerable?: boolean;
   }
 
-  declare var npm$namespace$Collection: {
-    extend: typeof Collection$extend,
-    isExtended: typeof Collection$isExtended
+  declare var npm$namespace$Refs$Collection: {
+    extend: typeof Refs$Collection$extend,
+    isExtended: typeof Refs$Collection$isExtended
   };
 
   /**
    * Extends a collection with Refs aware methods
    */
-  declare function Collection$extend(
+  declare function Refs$Collection$extend(
     collection: any[],
     refs: Refs,
     property: string | Refs$AttributeDescriptor,
     target: any
   ): any;
 
-  declare function Collection$isExtended(collection: any[]): boolean;
+  declare function Refs$Collection$isExtended(collection: any[]): boolean;
 }
