64ff31ae73536e466cd9d8302a63254ecec8fd56
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:38:49 +0500

    [leaflet-providers] update types

--- flow-types/types/leaflet-providers_vx.x.x/flow_v0.25.x-/leaflet-providers.js
+++ flow-types/types/leaflet-providers_vx.x.x/flow_v0.25.x-/leaflet-providers.js
@@ -1,38 +1,43 @@
-declare var npm$namespace$tileLayer: {
-  provider: typeof tileLayer$provider
-};
-declare function tileLayer$provider(
-  tileLayer$provider: string,
-  options?: TileLayerOptions & {
-    [name: string]: string
-  }
-): TileLayer$TileLayer$Provider;
-
-declare var npm$namespace$Provider: {
-  providers: typeof Provider$providers
-};
-declare var Provider$providers: Provider$ProvidersMap;
-
-declare interface Provider$ProvidersMap {
-  [providerName: string]: Provider$ProviderConfig;
-}
-
-declare interface Provider$ProviderConfig {
-  url: string;
-  options?: TileLayerOptions;
-  variants?: {
-    [variantName: string]: string | Provider$ProviderConfig
+declare module "leaflet" {
+  declare var npm$namespace$TileLayer: {
+    Provider: typeof TileLayer$Provider,
+    Provider: typeof npm$namespace$TileLayer$Provider
   };
-}
-declare class TileLayer$Provider mixins TileLayer {
-  constructor(
-    tileLayer$provider: string,
+  declare class TileLayer$Provider mixins TileLayer {
+    constructor(
+      provider: string,
+      options?: TileLayerOptions & {
+        [name: string]: string
+      }
+    ): this;
+  }
+
+  declare var npm$namespace$TileLayer$Provider: {
+    providers: typeof TileLayer$Provider$providers
+  };
+  declare var TileLayer$Provider$providers: Provider$ProvidersMap;
+
+  declare interface TileLayer$Provider$ProvidersMap {
+    [providerName: string]: Provider$ProviderConfig;
+  }
+
+  declare interface TileLayer$Provider$ProviderConfig {
+    url: string;
+    options?: TileLayerOptions;
+    variants?: {
+      [variantName: string]: string | TileLayer$Provider$ProviderConfig
+    };
+  }
+
+  declare var npm$namespace$tileLayer: {
+    provider: typeof tileLayer$provider
+  };
+  declare function tileLayer$provider(
+    provider: string,
     options?: TileLayerOptions & {
       [name: string]: string
     }
-  ): this;
-}
-declare module "leaflet" {
+  ): TileLayer$Provider;
 }
 declare module "leaflet-providers" {
   import typeof * as L from "leaflet";
