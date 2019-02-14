407f04c9c2eb8a77c22a927ba30786ab2a9fadc6
andretshurotshka <andwebar@gmail.com>
2019-02-14 13:12:26 +0500

    [zip.js] update types

--- flow-types/types/zip.js_vx.x.x/flow_v0.25.x-/zip.js.js
+++ flow-types/types/zip.js_vx.x.x/flow_v0.25.x-/zip.js.js
@@ -6,7 +6,20 @@ declare module "zip.js" {
     createWriter: typeof zip$createWriter,
     useWebWorkers: typeof zip$useWebWorkers,
     workerScriptsPath: typeof zip$workerScriptsPath,
-    workerScripts: typeof zip$workerScripts
+    workerScripts: typeof zip$workerScripts,
+
+    Reader: typeof zip$Reader,
+    TextReader: typeof zip$TextReader,
+    BlobReader: typeof zip$BlobReader,
+    Data64URIReader: typeof zip$Data64URIReader,
+    HttpReader: typeof zip$HttpReader,
+    ZipReader: typeof zip$ZipReader,
+    Writer: typeof zip$Writer,
+    TextWriter: typeof zip$TextWriter,
+    BlobWriter: typeof zip$BlobWriter,
+    FileWriter: typeof zip$FileWriter,
+    Data64URIWriter: typeof zip$Data64URIWriter,
+    ZipWriter: typeof zip$ZipWriter
   };
   declare export var zip$useWebWorkers: boolean;
 
@@ -28,30 +41,30 @@ declare module "zip.js" {
     ): void;
   }
 
-  declare export class zip$TextReader mixins zip$Reader {
+  declare export class zip$TextReader mixins Reader {
     constructor(text: string): this;
   }
 
-  declare export class zip$BlobReader mixins zip$Reader {
+  declare export class zip$BlobReader mixins Reader {
     constructor(blob: Blob): this;
   }
 
-  declare export class zip$Data64URIReader mixins zip$Reader {
+  declare export class zip$Data64URIReader mixins Reader {
     constructor(dataURI: string): this;
   }
 
-  declare export class zip$HttpReader mixins zip$Reader {
+  declare export class zip$HttpReader mixins Reader {
     constructor(url: string): this;
   }
 
   declare export function zip$createReader(
-    reader: zip$zip$Reader,
+    reader: zip$Reader,
     callback: (zipReader: zip$ZipReader) => void,
     onerror?: (error: any) => void
   ): void;
 
   declare export class zip$ZipReader {
-    getEntries(callback: (entries: zip$zip$Entry[]) => void): void;
+    getEntries(callback: (entries: zip$Entry[]) => void): void;
     close(callback?: () => void): void;
   }
 
@@ -65,7 +78,7 @@ declare module "zip.js" {
     comment: string;
     crc32: number;
     getData(
-      writer: zip$zip$Writer,
+      writer: zip$Writer,
       onend: (result: any) => void,
       onprogress?: (progress: number, total: number) => void,
       checkCrc32?: boolean
@@ -85,25 +98,25 @@ declare module "zip.js" {
     ): void;
   }
 
-  declare export class zip$TextWriter mixins zip$Writer {
+  declare export class zip$TextWriter mixins Writer {
     constructor(encoding: string): this;
   }
 
-  declare export class zip$BlobWriter mixins zip$Writer {
+  declare export class zip$BlobWriter mixins Writer {
     constructor(contentType: string): this;
   }
 
-  declare export class zip$FileWriter mixins zip$Writer {
+  declare export class zip$FileWriter mixins Writer {
     constructor(fileEntry: FileEntry): this;
   }
 
-  declare export class zip$Data64URIWriter mixins zip$Writer {
+  declare export class zip$Data64URIWriter mixins Writer {
     constructor(mimeString?: string): this;
   }
 
   declare export function zip$createWriter(
-    writer: zip$zip$Writer,
-    callback: (zipWriter: zip$zip$ZipWriter) => void,
+    writer: zip$Writer,
+    callback: (zipWriter: zip$ZipWriter) => void,
     onerror?: (error: any) => void,
     dontDeflate?: boolean
   ): void;
@@ -119,7 +132,7 @@ declare module "zip.js" {
   declare export class zip$ZipWriter {
     add(
       name: string,
-      reader: zip$zip$Reader,
+      reader: zip$Reader,
       onend: () => void,
       onprogress?: (progress: number, total: number) => void,
       options?: zip$WriteOptions
