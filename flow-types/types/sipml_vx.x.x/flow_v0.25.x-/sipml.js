53f78d9d481813a069102ceefb9c91113301eeda
andretshurotshka <andwebar@gmail.com>
2019-02-14 13:03:28 +0500

    [sipml] update types

--- flow-types/types/sipml_vx.x.x/flow_v0.25.x-/sipml.js
+++ flow-types/types/sipml_vx.x.x/flow_v0.25.x-/sipml.js
@@ -15,7 +15,14 @@ declare module "sipml" {
     isWebRtcSupported: typeof SIPml$isWebRtcSupported,
     isWebSocketSupported: typeof SIPml$isWebSocketSupported,
     setDebugLevel: typeof SIPml$setDebugLevel,
-    setWebRtcType: typeof SIPml$setWebRtcType
+    setWebRtcType: typeof SIPml$setWebRtcType,
+
+    Event: typeof SIPml$Event,
+    EventTarget: typeof SIPml$EventTarget,
+    Session: typeof SIPml$Session,
+    Stack: typeof SIPml$Stack,
+    Session: typeof npm$namespace$SIPml$Session,
+    Stack: typeof npm$namespace$SIPml$Stack
   };
   declare class SIPml$Event {
     description: string;
@@ -38,17 +45,23 @@ declare module "sipml" {
   }
 
   declare class SIPml$Session
-    mixins SIPml$EventTarget<
-        SIPml$Session.Stack$EventSubscriptionType,
-        SIPml$Session.SIPml$Event
-      > {
-    accept(configuration?: SIPml$Session.Stack$Configuration): number;
+    mixins EventTarget<Session$EventSubscriptionType, Session$Event> {
+    accept(configuration?: Session$Configuration): number;
     getId(): number;
     getRemoteFriendlyName(): string;
     getRemoteUri(): string;
-    reject(configuration?: SIPml$Session.Stack$Configuration): number;
-    setConfiguration(configuration?: SIPml$Session.Stack$Configuration): void;
+    reject(configuration?: Session$Configuration): number;
+    setConfiguration(configuration?: Session$Configuration): void;
   }
+
+  declare var npm$namespace$SIPml$Session: {
+    Call: typeof SIPml$Session$Call,
+    Event: typeof SIPml$Session$Event,
+    Message: typeof SIPml$Session$Message,
+    Publish: typeof SIPml$Session$Publish,
+    Registration: typeof SIPml$Session$Registration,
+    Subscribe: typeof SIPml$Session$Subscribe
+  };
 
   /**
    * Should be
@@ -70,17 +83,19 @@ declare module "sipml" {
    * "message_error" |
    * "webrtc_error" |
    */
-  declare type Session$EventSubscriptionType = string;
+  declare type SIPml$Session$EventSubscriptionType = string;
 
-  declare interface Session$Configuration {
+  declare interface SIPml$Session$Configuration {
     audio_remote?: HTMLElement;
     bandwidth?: {
       audio: number,
       video: number
     };
     events_listener?: {
-      events: Session$EventSubscriptionType | Session$EventSubscriptionType[],
-      listener: (e: SIPml$Session.SIPml$Event) => void
+      events:
+        | SIPml$Session$EventSubscriptionType
+        | SIPml$Session$EventSubscriptionType[],
+      listener: (e: Session$Event) => void
     };
     expires?: number;
     from?: string;
@@ -94,49 +109,16 @@ declare module "sipml" {
       minHeight?: number,
       maxHeight?: number
     };
-    bandwidth?: {
-      audio: number,
-      video: number
-    };
-    display_name?: string;
-    enable_click2call?: boolean;
-    enable_early_ims?: boolean;
-    enable_media_stream_cache?: boolean;
-    enable_rtcweb_breaker?: boolean;
-    events_listener?: {
-      events: Session$EventSubscriptionType | Session$EventSubscriptionType[],
-      listener: (e: SIPml$Stack.SIPml$Event) => void
-    };
-    ice_servers?: Object[];
-    impi?: string;
-    impu?: string;
-    outbound_proxy_url?: string;
-    password?: string;
-    realm?: string;
-    sip_headers?: Object[];
-    video_size?: {
-      minWidth?: number,
-      maxWidth?: number,
-      minHeight?: number,
-      maxHeight?: number
-    };
-    websocket_proxy_url?: string;
   }
 
-  declare class Session$Call
-    mixins SIPml$Session,
-      SIPml$EventTarget<
-        Session$Call.Session$EventSubscriptionType,
-        SIPml$Session.SIPml$Event
-      > {
-    acceptTransfer(configuration?: SIPml$Session.Session$Configuration): number;
-    call(
-      to: string,
-      configuration?: SIPml$Session.Session$Configuration
-    ): number;
+  declare class SIPml$Session$Call
+    mixins Session,
+      EventTarget<Session$Call$EventSubscriptionType, Session$Event> {
+    acceptTransfer(configuration?: Session$Configuration): number;
+    call(to: string, configuration?: Session$Configuration): number;
     dtmf(): number;
-    hangup(configuration?: SIPml$Session.Session$Configuration): number;
-    hold(configuration?: SIPml$Session.Session$Configuration): number;
+    hangup(configuration?: Session$Configuration): number;
+    hold(configuration?: Session$Configuration): number;
     info(): number;
     rejectTransfer(): number;
     resume(): number;
@@ -146,147 +128,111 @@ declare module "sipml" {
   /**
    * Should be
    *
-   * "*" |
-   * "connecting" |
-   * "connected" |
-   * "terminating" |
-   * "terminated" |
-   * "i_ao_request" |
-   * "media_added" |
-   * "media_removed" |
-   * "i_request" |
-   * "o_request" |
-   * "cancelled_request" |
-   * "sent_request" |
-   * "transport_error" |
-   * "global_error" |
-   * "message_error" |
-   * "webrtc_error" |
+   * Session.EventSubscriptionType |
+   * "m_early_media" |
+   * "m_local_hold_ok" |
+   * "m_local_hold_nok" |
+   * "m_local_resume_ok" |
+   * "m_local_resume_nok" |
+   * "m_remote_hold" |
+   * "m_remote_resume" |
+   * "m_stream_video_local_added" |
+   * "m_stream_video_local_removed" |
+   * "m_stream_video_remote_added" |
+   * "m_stream_video_remote_removed" |
+   * "m_stream_audio_local_added" |
+   * "m_stream_audio_local_removed" |
+   * "m_stream_audio_remote_added" |
+   * "m_stream_audio_remote_removed" |
+   * "i_ect_new_call" |
+   * "o_ect_trying" |
+   * "o_ect_accepted" |
+   * "o_ect_completed" |
+   * "i_ect_completed" |
+   * "o_ect_failed" |
+   * "i_ect_failed" |
+   * "o_ect_notify" |
+   * "i_ect_notify" |
+   * "i_ect_requested " |
+   * "m_bfcp_info" |
+   * "i_info" |
    */
-  declare type Call$EventSubscriptionType = string;
+  declare type SIPml$Session$Call$EventSubscriptionType = Session$EventSubscriptionType;
 
-  declare class Session$Event mixins SIPml$Session$Event {
+  declare class SIPml$Session$Event mixins SIPml$Event {
     session: SIPml$Session;
     getTransferDestinationFriendlyName(): string;
   }
 
-  declare class Session$Message mixins SIPml$Session {
+  declare class SIPml$Session$Message mixins Session {
     send(
       to: string,
       content?: any,
       contentType?: string,
-      configuration?: SIPml$Session.Session$Configuration
+      configuration?: Session$Configuration
     ): number;
   }
 
-  declare class Session$Publish mixins SIPml$Session {
+  declare class SIPml$Session$Publish mixins Session {
     publish(
       content?: any,
       contentType?: string,
-      configuration?: SIPml$Session.Session$Configuration
+      configuration?: Session$Configuration
     ): number;
-    unpublish(configuration?: SIPml$Session.Session$Configuration): void;
+    unpublish(configuration?: Session$Configuration): void;
   }
 
-  declare class Session$Registration mixins SIPml$Session {
-    register(configuration?: SIPml$Session.Session$Configuration): void;
-    unregister(configuration?: SIPml$Session.Session$Configuration): void;
+  declare class SIPml$Session$Registration mixins Session {
+    register(configuration?: Session$Configuration): void;
+    unregister(configuration?: Session$Configuration): void;
   }
 
-  declare class Session$Subscribe
-    mixins SIPml$Session,
-      SIPml$EventTarget<
-        Session$Subscribe.Call$EventSubscriptionType,
-        SIPml$Session.Session$Event
-      > {
-    subscribe(
-      to: string,
-      configuration?: SIPml$Session.Session$Configuration
-    ): number;
-    unsubscribe(configuration?: SIPml$Session.Session$Configuration): number;
+  declare class SIPml$Session$Subscribe
+    mixins Session,
+      EventTarget<Session$Subscribe$EventSubscriptionType, Session$Event> {
+    subscribe(to: string, configuration?: Session$Configuration): number;
+    unsubscribe(configuration?: Session$Configuration): number;
   }
 
   /**
    * Should be
    *
-   * "*" |
-   * "connecting" |
-   * "connected" |
-   * "terminating" |
-   * "terminated" |
-   * "i_ao_request" |
-   * "media_added" |
-   * "media_removed" |
-   * "i_request" |
-   * "o_request" |
-   * "cancelled_request" |
-   * "sent_request" |
-   * "transport_error" |
-   * "global_error" |
-   * "message_error" |
-   * "webrtc_error" |
+   * Session.EventSubscriptionType | "i_notify"
    */
-  declare type Subscribe$EventSubscriptionType = string;
+  declare type SIPml$Session$Subscribe$EventSubscriptionType = Session$EventSubscriptionType;
 
   declare class SIPml$Stack
-    mixins SIPml$EventTarget<
-        SIPml$Stack.Subscribe$EventSubscriptionType,
-        SIPml$Stack.Session$Event
-      > {
-    constructor(configuration?: SIPml$Stack.Session$Configuration): this;
-    setConfiguration(configuration: SIPml$Stack.Session$Configuration): number;
-    newSession(
-      type: string,
-      configuration?: SIPml$Session.Session$Configuration
-    ): any;
+    mixins EventTarget<Stack$EventSubscriptionType, Stack$Event> {
+    constructor(configuration?: Stack$Configuration): this;
+    setConfiguration(configuration: Stack$Configuration): number;
+    newSession(type: string, configuration?: Session$Configuration): any;
     start(): number;
     stop(timeout?: number): number;
   }
 
+  declare var npm$namespace$SIPml$Stack: {
+    Event: typeof SIPml$Stack$Event
+  };
+
   /**
    * Should be
    *
    * "*" |
-   * "connecting" |
-   * "connected" |
-   * "terminating" |
-   * "terminated" |
-   * "i_ao_request" |
-   * "media_added" |
-   * "media_removed" |
-   * "i_request" |
-   * "o_request" |
-   * "cancelled_request" |
-   * "sent_request" |
-   * "transport_error" |
-   * "global_error" |
-   * "message_error" |
-   * "webrtc_error" |
+   * "starting" |
+   * "started" |
+   * "stopping" |
+   * "stopped" |
+   * "failed_to_start" |
+   * "failed_to_stop" |
+   * "i_new_call" |
+   * "i_new_message" |
+   * "m_permission_requested" |
+   * "m_permission_accepted" |
+   * "m_permission_refused";
    */
-  declare type Stack$EventSubscriptionType = string;
+  declare type SIPml$Stack$EventSubscriptionType = string;
 
-  declare interface Stack$Configuration {
-    audio_remote?: HTMLElement;
-    bandwidth?: {
-      audio: number,
-      video: number
-    };
-    events_listener?: {
-      events: Stack$EventSubscriptionType | Stack$EventSubscriptionType[],
-      listener: (e: SIPml$Session.Session$Event) => void
-    };
-    expires?: number;
-    from?: string;
-    sip_caps?: Object[];
-    sip_headers?: Object[];
-    video_local?: HTMLElement;
-    video_remote?: HTMLElement;
-    video_size?: {
-      minWidth?: number,
-      maxWidth?: number,
-      minHeight?: number,
-      maxHeight?: number
-    };
+  declare interface SIPml$Stack$Configuration {
     bandwidth?: {
       audio: number,
       video: number
@@ -297,8 +243,10 @@ declare module "sipml" {
     enable_media_stream_cache?: boolean;
     enable_rtcweb_breaker?: boolean;
     events_listener?: {
-      events: Stack$EventSubscriptionType | Stack$EventSubscriptionType[],
-      listener: (e: SIPml$Stack.Session$Event) => void
+      events:
+        | SIPml$Stack$EventSubscriptionType
+        | SIPml$Stack$EventSubscriptionType[],
+      listener: (e: Stack$Event) => void
     };
     ice_servers?: Object[];
     impi?: string;
@@ -316,7 +264,7 @@ declare module "sipml" {
     websocket_proxy_url?: string;
   }
 
-  declare class Stack$Event mixins SIPml$Stack$Event {
+  declare class SIPml$Stack$Event mixins SIPml$Event {
     description: string;
     newSession: SIPml$Session;
     type: string;
