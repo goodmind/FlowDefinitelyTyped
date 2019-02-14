5712eb665d650cf9caea22b08c7acf017a700619
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:05:01 +0500

    [@xmpp/xml] update types

--- flow-types/types/@xmpp/xml_vx.x.x/flow_v0.25.x-/xml.js
+++ flow-types/types/@xmpp/xml_vx.x.x/flow_v0.25.x-/xml.js
@@ -1,71 +1,12 @@
-declare interface JSX$IntrinsicElements {
-  [elemName: string]: any;
-}
-
-declare type JSX$Element = xml$JSX$Element;
 declare module "global" {
-}
+  declare interface JSX$IntrinsicElements {
+    [elemName: string]: any;
+  }
 
-declare var npm$namespace$xml: {
-  x: typeof xml$x,
-  escapeXML: typeof xml$escapeXML,
-  unescapeXML: typeof xml$unescapeXML,
-  escapeXMLText: typeof xml$escapeXMLText,
-  unescapeXMLText: typeof xml$unescapeXMLText
-};
-declare function xml$x(
-  name: string,
-  attrs?:
-    | string
-    | {
-        [key: string]: any
-      },
-  ...children: xml$ElementChild[]
-): JSX$Element;
-
-declare var xml$escapeXML: typeof undefined;
-
-declare var xml$unescapeXML: typeof undefined;
-
-declare var xml$escapeXMLText: typeof undefined;
-
-declare var xml$unescapeXMLText: typeof undefined;
-
-declare class xml$Element mixins ltx.xml$Element {
-  attrs: {
-    [key: string]: any
-  };
-  append(nodes: xml$ElementChild): xml$Element;
-  prepend(nodes: xml$ElementChild): xml$Element;
-}
-
-declare type xml$ElementChild =
-  | xml$Element
-  | xml$Element[]
-  | string
-  | number
-  | boolean;
-
-declare class xml$Parser mixins EventEmitter {
-  XMLError: typeof XMLError;
-  parser: ltx.xml$Parser;
-  root: xml$Element | null;
-  cursor: xml$Element | null;
-  onStartElement(
-    name: string,
-    attrs?:
-      | string
-      | {
-          [attrName: string]: any
-        }
-  ): void;
-  onEndElement(name: string): void;
-  onText(str: string): void;
-  write(data: Buffer): void;
-  end(data: Buffer): void;
+  declare type JSX$Element = xml$Element;
 }
 declare module "@xmpp/xml" {
-  declare module.exports: typeof xml;
+  declare export default typeof xml;
 
   import typeof * as ltx from "ltx";
 
@@ -80,9 +21,70 @@ declare module "@xmpp/xml" {
       | {
           [key: string]: any
         },
-    ...children: xml$xml$ElementChild[]
-  ): xml$xml$Element;
+    ...children: xml$ElementChild[]
+  ): xml$Element;
 
+  declare var npm$namespace$xml: {
+    x: typeof xml$x,
+    escapeXML: typeof xml$escapeXML,
+    unescapeXML: typeof xml$unescapeXML,
+    escapeXMLText: typeof xml$escapeXMLText,
+    unescapeXMLText: typeof xml$unescapeXMLText,
+
+    Element: typeof xml$Element,
+    Parser: typeof xml$Parser
+  };
+  declare function xml$x(
+    name: string,
+    attrs?:
+      | string
+      | {
+          [key: string]: any
+        },
+    ...children: xml$ElementChild[]
+  ): JSX$Element;
+
+  declare var xml$escapeXML: typeof escape.escapeXML;
+
+  declare var xml$unescapeXML: typeof escape.unescapeXML;
+
+  declare var xml$escapeXMLText: typeof escape.escapeXMLText;
+
+  declare var xml$unescapeXMLText: typeof escape.unescapeXMLText;
+
+  declare class xml$Element mixins ltx.Element {
+    attrs: {
+      [key: string]: any
+    };
+    append(nodes: xml$ElementChild): xml$Element;
+    prepend(nodes: xml$ElementChild): xml$Element;
+  }
+
+  declare type xml$ElementChild =
+    | xml$Element
+    | xml$Element[]
+    | string
+    | number
+    | boolean;
+
+  declare class xml$Parser mixins EventEmitter {
+    XMLError: typeof XMLError;
+    parser: ltx.Parser;
+    root: xml$Element | null;
+    cursor: xml$Element | null;
+    onStartElement(
+      name: string,
+      attrs?:
+        | string
+        | {
+            [attrName: string]: any
+          }
+    ): void;
+    onEndElement(name: string): void;
+    onText(str: string): void;
+    write(data: Buffer): void;
+    end(data: Buffer): void;
+  }
   declare class XMLError mixins Error {
     name: "XMLError";
   }
