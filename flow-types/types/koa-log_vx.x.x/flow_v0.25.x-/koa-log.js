40f9433ac193ddb4d44174c5615f429de2b3346d
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:38:03 +0500

    [koa-log] update types

--- flow-types/types/koa-log_vx.x.x/flow_v0.25.x-/koa-log.js
+++ flow-types/types/koa-log_vx.x.x/flow_v0.25.x-/koa-log.js
@@ -3,40 +3,47 @@ declare module "koa-log" {
 
   declare function koaLog(type: string): Koa.Middleware;
 
-  declare var npm$namespace$morgan: {
-    cookie: typeof morgan$cookie,
-    date: typeof morgan$date,
-    format: typeof morgan$format,
-    length: typeof morgan$length,
-    method: typeof morgan$method,
-    referrer: typeof morgan$referrer,
-    status: typeof morgan$status,
-    token: typeof morgan$token,
-    url: typeof morgan$url
+  declare var npm$namespace$koaLog: {
+    morgan: typeof npm$namespace$koaLog$morgan
   };
-  declare function morgan$cookie(ctx: Koa.BaseContext, key: string): string;
 
-  declare function morgan$date(
+  declare var npm$namespace$koaLog$morgan: {
+    cookie: typeof koaLog$morgan$cookie,
+    date: typeof koaLog$morgan$date,
+    format: typeof koaLog$morgan$format,
+    length: typeof koaLog$morgan$length,
+    method: typeof koaLog$morgan$method,
+    referrer: typeof koaLog$morgan$referrer,
+    status: typeof koaLog$morgan$status,
+    token: typeof koaLog$morgan$token,
+    url: typeof koaLog$morgan$url
+  };
+  declare function koaLog$morgan$cookie(
     ctx: Koa.BaseContext,
-    morgan$format: string
+    key: string
   ): string;
 
-  declare function morgan$format(name: string): string;
+  declare function koaLog$morgan$date(
+    ctx: Koa.BaseContext,
+    format: string
+  ): string;
 
-  declare function morgan$length(ctx: Koa.BaseContext): number | void;
+  declare function koaLog$morgan$format(name: string): string;
 
-  declare function morgan$method(ctx: Koa.BaseContext): string;
+  declare function koaLog$morgan$length(ctx: Koa.BaseContext): number | void;
 
-  declare function morgan$referrer(ctx: Koa.BaseContext): string;
+  declare function koaLog$morgan$method(ctx: Koa.BaseContext): string;
 
-  declare function morgan$status(ctx: Koa.BaseContext): number;
+  declare function koaLog$morgan$referrer(ctx: Koa.BaseContext): string;
 
-  declare function morgan$token(
+  declare function koaLog$morgan$status(ctx: Koa.BaseContext): number;
+
+  declare function koaLog$morgan$token(
     name: string,
     fn: (ctx: Koa.BaseContext) => string
   ): void;
 
-  declare function morgan$url(ctx: Koa.BaseContext): string;
+  declare function koaLog$morgan$url(ctx: Koa.BaseContext): string;
 
-  declare module.exports: typeof koaLog;
+  declare export default typeof koaLog;
 }
