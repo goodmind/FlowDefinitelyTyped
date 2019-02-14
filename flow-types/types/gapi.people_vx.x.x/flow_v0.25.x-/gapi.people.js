4d1ce31537a411e060651912df07f3378c51be17
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:26:50 +0500

    [gapi.people] update types

--- flow-types/types/gapi.people_vx.x.x/flow_v0.25.x-/gapi.people.js
+++ flow-types/types/gapi.people_vx.x.x/flow_v0.25.x-/gapi.people.js
@@ -1,103 +1,117 @@
 declare module "gapi.people" {
-  declare var npm$namespace$people: {
-    get: typeof people$get,
-    getBatchGet: typeof people$getBatchGet
+  declare var npm$namespace$gapi: {
+    client: typeof npm$namespace$gapi$client
   };
-  declare interface people$GetParameters {
+
+  declare var npm$namespace$gapi$client: {
+    people: typeof npm$namespace$gapi$client$people
+  };
+
+  declare var npm$namespace$gapi$client$people: {
+    people: typeof npm$namespace$gapi$client$people$people
+  };
+
+  declare var npm$namespace$gapi$client$people$people: {
+    get: typeof gapi$client$people$people$get,
+    getBatchGet: typeof gapi$client$people$people$getBatchGet,
+
+    connections: typeof npm$namespace$gapi$client$people$people$connections
+  };
+  declare interface gapi$client$people$people$GetParameters {
     resourceName: string;
     personFields: string;
   }
 
-  declare function people$get(
-    parameters: people$GetParameters
+  declare function gapi$client$people$people$get(
+    parameters: gapi$client$people$people$GetParameters
   ): HttpRequest<people$Person>;
 
-  declare interface people$GetBatchGetParameters {
+  declare interface gapi$client$people$people$GetBatchGetParameters {
     resourcesName?: string;
     personFields: string;
   }
 
-  declare function people$getBatchGet(
-    parameters: people$GetBatchGetParameters
+  declare function gapi$client$people$people$getBatchGet(
+    parameters: gapi$client$people$people$GetBatchGetParameters
   ): HttpRequest<people$BatchGetResponse>;
 
-  declare interface people$BatchGetResponse {
+  declare interface gapi$client$people$people$BatchGetResponse {
     responses: people$PersonResponse[];
   }
 
-  declare interface people$PersonResponse {
+  declare interface gapi$client$people$people$PersonResponse {
     httpStatusCode: number;
     person: people$Person;
     requestedResourceName: string;
   }
 
-  declare var npm$namespace$connections: {
-    list: typeof connections$list
+  declare var npm$namespace$gapi$client$people$people$connections: {
+    list: typeof gapi$client$people$people$connections$list
   };
-  declare function connections$list(
+  declare function gapi$client$people$people$connections$list(
     parameters: connections$ListParameters
   ): HttpRequest<connections$Response>;
 
-  declare type connections$SortOrder =
+  declare type gapi$client$people$people$connections$SortOrder =
     | "LAST_MODIFIED_ASCENDING"
     | "FIRST_NAME_ASCENDING"
     | "LAST_NAME_ASCENDING";
 
-  declare interface connections$ListParameters {
+  declare interface gapi$client$people$people$connections$ListParameters {
     resourceName: string;
     pageToken?: string;
     pageSize?: number;
-    sortOrder?: connections$SortOrder;
+    sortOrder?: gapi$client$people$people$connections$SortOrder;
     syncToken?: string;
     personFields: string;
   }
 
-  declare interface connections$Response {
+  declare interface gapi$client$people$people$connections$Response {
     connections: people$Person[];
     nextPageToken: string;
     nextSyncToken: string;
   }
 
-  declare type people$SourceType =
+  declare type gapi$client$people$SourceType =
     | "SOURCE_TYPE_UNSPECIFIED"
     | "ACCOUNT"
     | "PROFILE"
     | "DOMAIN_PROFILE"
     | "CONTACT";
 
-  declare interface people$Source {
-    type: people$SourceType;
+  declare interface gapi$client$people$Source {
+    type: gapi$client$people$SourceType;
     id: string;
     etag: string;
     resourceName: string;
   }
 
-  declare type people$ObjectType =
+  declare type gapi$client$people$ObjectType =
     | "OBJECT_TYPE_UNSPECIFIED"
     | "PERSON"
     | "PAGE";
 
-  declare interface people$PersonMetadata {
-    sources: people$Source[];
+  declare interface gapi$client$people$PersonMetadata {
+    sources: gapi$client$people$Source[];
     previousResourceNames: string[];
     linkedPeopleResourceNames: string[];
     deleted: boolean;
-    objectType: people$ObjectType;
+    objectType: gapi$client$people$ObjectType;
   }
 
-  declare interface people$FieldMetadata {
+  declare interface gapi$client$people$FieldMetadata {
     primary: boolean;
     verified: boolean;
-    source: people$Source;
+    source: gapi$client$people$Source;
   }
 
-  declare interface people$Locale {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$Locale {
+    metadata: gapi$client$people$FieldMetadata;
     value: string;
   }
 
-  declare interface people$Name {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$Name {
+    metadata: gapi$client$people$FieldMetadata;
     displayName: string;
     displayNameLastFirst: string;
     familyName: string;
@@ -113,39 +127,39 @@ declare module "gapi.people" {
     phoneticHonorificSuffix: string;
   }
 
-  declare type people$NicknameType =
+  declare type gapi$client$people$NicknameType =
     | "DEFAULT"
     | "MAIDEN_NAME"
     | "INITIALS"
     | "GPLUS"
     | "OTHER_NAME";
 
-  declare interface people$Nickname {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$Nickname {
+    metadata: gapi$client$people$FieldMetadata;
     value: string;
-    type: people$NicknameType;
+    type: gapi$client$people$NicknameType;
   }
 
-  declare interface people$CoverPhoto {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$CoverPhoto {
+    metadata: gapi$client$people$FieldMetadata;
     url: string;
   }
 
-  declare interface people$Photo {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$Photo {
+    metadata: gapi$client$people$FieldMetadata;
     url: string;
   }
 
-  declare interface people$Gender {}
+  declare interface gapi$client$people$Gender {}
 
-  declare interface people$AgeRange {}
+  declare interface gapi$client$people$AgeRange {}
 
-  declare interface people$Birthday {}
+  declare interface gapi$client$people$Birthday {}
 
-  declare interface people$Event {}
+  declare interface gapi$client$people$Event {}
 
-  declare interface people$Address {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$Address {
+    metadata: gapi$client$people$FieldMetadata;
     formattedValue: string;
     type: string;
     formattedType: string;
@@ -159,83 +173,83 @@ declare module "gapi.people" {
     countryCode: string;
   }
 
-  declare interface people$Residence {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$Residence {
+    metadata: gapi$client$people$FieldMetadata;
     value: string;
     current: boolean;
   }
 
-  declare interface people$EmailAddress {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$EmailAddress {
+    metadata: gapi$client$people$FieldMetadata;
     value: string;
     type: string;
     formattedType: string;
     displayName: string;
   }
 
-  declare interface people$PhoneNumber {
-    metadata: people$FieldMetadata;
+  declare interface gapi$client$people$PhoneNumber {
+    metadata: gapi$client$people$FieldMetadata;
     value: string;
     canonicalForm: string;
     type: string;
     formattedType: string;
   }
 
-  declare interface people$ImClient {}
+  declare interface gapi$client$people$ImClient {}
 
-  declare interface people$Tagline {}
+  declare interface gapi$client$people$Tagline {}
 
-  declare interface people$Biography {}
+  declare interface gapi$client$people$Biography {}
 
-  declare interface people$Url {}
+  declare interface gapi$client$people$Url {}
 
-  declare interface people$Organization {}
+  declare interface gapi$client$people$Organization {}
 
-  declare interface people$Occupation {}
+  declare interface gapi$client$people$Occupation {}
 
-  declare interface people$Interest {}
+  declare interface gapi$client$people$Interest {}
 
-  declare interface people$Skill {}
+  declare interface gapi$client$people$Skill {}
 
-  declare interface people$BraggingRights {}
+  declare interface gapi$client$people$BraggingRights {}
 
-  declare interface people$Relation {}
+  declare interface gapi$client$people$Relation {}
 
-  declare interface people$RelationshipInterest {}
+  declare interface gapi$client$people$RelationshipInterest {}
 
-  declare interface people$RelationshipStatus {}
+  declare interface gapi$client$people$RelationshipStatus {}
 
-  declare interface people$Membership {}
+  declare interface gapi$client$people$Membership {}
 
-  declare interface people$Person {
+  declare interface gapi$client$people$Person {
     resourceName: string;
     etag: string;
-    metadata: people$PersonMetadata;
-    locales: people$Locale[];
-    names: people$Name[];
-    nicknames?: people$Nickname[];
-    coverPhotos: people$CoverPhoto[];
-    photos?: people$Photo[];
-    genders?: people$Gender[];
-    ageRange?: people$AgeRange;
-    birthdays?: people$Birthday[];
-    events?: people$Event[];
-    addresses?: people$Address[];
-    residences?: people$Residence[];
-    emailAddresses?: people$EmailAddress[];
-    phoneNumbers?: people$PhoneNumber[];
-    imClients?: people$ImClient[];
-    taglines?: people$Tagline[];
-    biographies?: people$Biography[];
-    urls?: people$Url[];
-    organizations?: people$Organization[];
-    occupations?: people$Occupation[];
-    interests?: people$Interest[];
-    skills?: people$Skill[];
-    people$BraggingRights?: people$BraggingRights[];
-    relations?: people$Relation[];
-    relationshipInterests?: people$RelationshipInterest[];
-    relationshipStatuses?: people$RelationshipStatus[];
-    memberships?: people$Membership[];
+    metadata: gapi$client$people$PersonMetadata;
+    locales: gapi$client$people$Locale[];
+    names: gapi$client$people$Name[];
+    nicknames?: gapi$client$people$Nickname[];
+    coverPhotos: gapi$client$people$CoverPhoto[];
+    photos?: gapi$client$people$Photo[];
+    genders?: gapi$client$people$Gender[];
+    ageRange?: gapi$client$people$AgeRange;
+    birthdays?: gapi$client$people$Birthday[];
+    events?: gapi$client$people$Event[];
+    addresses?: gapi$client$people$Address[];
+    residences?: gapi$client$people$Residence[];
+    emailAddresses?: gapi$client$people$EmailAddress[];
+    phoneNumbers?: gapi$client$people$PhoneNumber[];
+    imClients?: gapi$client$people$ImClient[];
+    taglines?: gapi$client$people$Tagline[];
+    biographies?: gapi$client$people$Biography[];
+    urls?: gapi$client$people$Url[];
+    organizations?: gapi$client$people$Organization[];
+    occupations?: gapi$client$people$Occupation[];
+    interests?: gapi$client$people$Interest[];
+    skills?: gapi$client$people$Skill[];
+    BraggingRights?: gapi$client$people$BraggingRights[];
+    relations?: gapi$client$people$Relation[];
+    relationshipInterests?: gapi$client$people$RelationshipInterest[];
+    relationshipStatuses?: gapi$client$people$RelationshipStatus[];
+    memberships?: gapi$client$people$Membership[];
   }
 }
