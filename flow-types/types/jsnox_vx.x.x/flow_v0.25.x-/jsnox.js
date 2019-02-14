424830792c2c90dacd723aa87d2874888364c6e1
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:36:14 +0500

    [jsnox] update types

--- flow-types/types/jsnox_vx.x.x/flow_v0.25.x-/jsnox.js
+++ flow-types/types/jsnox_vx.x.x/flow_v0.25.x-/jsnox.js
@@ -11,27 +11,27 @@ declare module "jsnox" {
   declare type Module = (reactObj: ReactLikeObject) => CreateElement;
   declare interface CreateElement {
     /**
- * Renders an HTML element from the given spec string, with children but without
- * extra props.
- * @param specString A string that defines a component in a way that resembles
-CSS selectors. Eg. "input:email#foo.bar.baz[name=email][required]"
- * @param children A single React node (string or ReactElement) or array of nodes.
-Note that unlike with React itself, multiple children must be placed into an array.
- */
+     * Renders an HTML element from the given spec string, with children but without
+     * extra props.
+     * @param specString A string that defines a component in a way that resembles
+     * CSS selectors. Eg. "input:email#foo.bar.baz[name=email][required]"
+     * @param children A single React node (string or ReactElement) or array of nodes.
+     * Note that unlike with React itself, multiple children must be placed into an array.
+     */
     <P>(
       specString: string,
       children: React.ReactNode
     ): React.DOMElement<P, Element>;
 
     /**
- * Renders an HTML element from the given spec string, with optional props
- * and children
- * @param specString A string that defines a component in a way that resembles
-CSS selectors. Eg. "input:email#foo.bar.baz[name=email][required]"
- * @param props Object of html attribute key-value pairs
- * @param children A single React node (string or ReactElement) or array of nodes.
-Note that unlike with React itself, multiple children must be placed into an array.
- */
+     * Renders an HTML element from the given spec string, with optional props
+     * and children
+     * @param specString A string that defines a component in a way that resembles
+     * CSS selectors. Eg. "input:email#foo.bar.baz[name=email][required]"
+     * @param props Object of html attribute key-value pairs
+     * @param children A single React node (string or ReactElement) or array of nodes.
+     * Note that unlike with React itself, multiple children must be placed into an array.
+     */
     <P>(
       specString: string,
       props?: React.HTMLAttributes<{}>,
@@ -39,25 +39,25 @@ Note that unlike with React itself, multiple children must be placed into an arr
     ): React.DOMElement<P, Element>;
 
     /**
- * Renders a React component, with children but no props
- * @param component A plain React component (created from React.createClass()) or
-component factory (created from React.createFactory())
- * @param children A single React node (string or ReactElement) or array of nodes.
-Note that unlike with React itself, multiple children must be placed into an array.
- */
+     * Renders a React component, with children but no props
+     * @param component A plain React component (created from React.createClass()) or
+     * component factory (created from React.createFactory())
+     * @param children A single React node (string or ReactElement) or array of nodes.
+     * Note that unlike with React itself, multiple children must be placed into an array.
+     */
     <P>(
       component: React.ComponentClass<P>,
       children: React.ReactNode
     ): React.ReactElement<P>;
 
     /**
- * Renders a React component, with optional props and children
- * @param component A plain React component (created from React.createClass()) or
-component factory (created from React.createFactory())
- * @param props Props object to pass to the component
- * @param children A single React node (string or ReactElement) or array of nodes.
-Note that unlike with React itself, multiple children must be placed into an array.
- */
+     * Renders a React component, with optional props and children
+     * @param component A plain React component (created from React.createClass()) or
+     * component factory (created from React.createFactory())
+     * @param props Props object to pass to the component
+     * @param children A single React node (string or ReactElement) or array of nodes.
+     * Note that unlike with React itself, multiple children must be placed into an array.
+     */
     <P>(
       component: React.ComponentClass<P>,
       props?: P,
@@ -65,5 +65,5 @@ Note that unlike with React itself, multiple children must be placed into an arr
     ): React.ReactElement<P>;
   }
   declare var exports: Module;
-  declare module.exports: typeof exports;
+  declare export default typeof exports;
 }
