3d8926a661f51dea16da9ec9481b018840da2558
andretshurotshka <andwebar@gmail.com>
2019-02-14 13:12:45 +0500

    [gulp-template] update types

--- flow-types/unformatted/gulp-template_vx.x.x/flow_v0.25.x-/gulp-template.js
+++ flow-types/unformatted/gulp-template_vx.x.x/flow_v0.25.x-/gulp-template.js
@@ -1,15 +1,19 @@
-
-      declare var npm$namespace$gulpTemplate: {
-        precompile: typeof gulpTemplate$precompile,
-        
-      }
-declare function gulpTemplate$precompile(options: _.TemplateOptions): stream.Transform
 declare module 'gulp-template' {
         import typeof * as _ from 'lodash';
 
 	import typeof * as stream from 'stream';
 
 	declare function gulpTemplate(data: any, options: _.TemplateOptions): stream.Transform
+
+	
+      declare var npm$namespace$gulpTemplate: {
+        precompile: typeof gulpTemplate$precompile,
+        
+        
+        
+        
+      }
+declare function gulpTemplate$precompile(options: _.TemplateOptions): stream.Transform
 
 	declare export default typeof gulpTemplate
 
