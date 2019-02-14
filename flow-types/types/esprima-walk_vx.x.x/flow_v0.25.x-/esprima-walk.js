38389f87ccd689dc7881206db44e3e747694f56c
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:22:02 +0500

    [esprima-walk] update types

--- flow-types/types/esprima-walk_vx.x.x/flow_v0.25.x-/esprima-walk.js
+++ flow-types/types/esprima-walk_vx.x.x/flow_v0.25.x-/esprima-walk.js
@@ -1,28 +1,3 @@
-declare var npm$namespace$walk: {
-  walk: typeof walk$walk,
-  walkAddParent: typeof walk$walkAddParent
-};
-
-/**
- * Walk the provided AST; fn is called once for each node with a `type`
- * @param {ESTree.Program} ast program to walk
- * @param {function} fn function invoked for each node
- */
-declare export function walk$walk(
-  ast: ESTree.Program,
-  fn: (node: ESTree.Node) => void
-): void;
-
-/**
- * Walk the provided AST; fn is called once for each node with a `type`.
- * Adds a parent property prior to invoking fn when applicable
- * @param {ESTree.Program} ast program to walk
- * @param {function} fn function invoked for each node
- */
-declare export function walk$walkAddParent(
-  ast: ESTree.Program,
-  fn: (node: NodeWithParent) => void
-): void;
 declare module "esprima-walk" {
   import typeof * as ESTree from "estree";
 
@@ -40,5 +15,31 @@ declare module "esprima-walk" {
     fn: (node: ESTree.Node) => void
   ): void;
 
-  declare module.exports: typeof walk$walk;
+  declare var npm$namespace$walk: {
+    walk: typeof walk$walk,
+    walkAddParent: typeof walk$walkAddParent
+  };
+
+  /**
+   * Walk the provided AST; fn is called once for each node with a `type`
+   * @param {ESTree.Program} ast program to walk
+   * @param {function} fn function invoked for each node
+   */
+  declare export function walk$walk(
+    ast: ESTree.Program,
+    fn: (node: ESTree.Node) => void
+  ): void;
+
+  /**
+   * Walk the provided AST; fn is called once for each node with a `type`.
+   * Adds a parent property prior to invoking fn when applicable
+   * @param {ESTree.Program} ast program to walk
+   * @param {function} fn function invoked for each node
+   */
+  declare export function walk$walkAddParent(
+    ast: ESTree.Program,
+    fn: (node: NodeWithParent) => void
+  ): void;
+
+  declare export default typeof walk;
 }
