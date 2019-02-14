b3440af8e4fe16c3d9a41cd34a0eb090aeae47b2
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:08:02 +0500

    [argon2-browser] update types

--- flow-types/types/argon2-browser_vx.x.x/flow_v0.25.x-/argon2-browser.js
+++ flow-types/types/argon2-browser_vx.x.x/flow_v0.25.x-/argon2-browser.js
@@ -1,6 +1,8 @@
 declare module "argon2-browser" {
   declare var npm$namespace$argon2: {
-    hash: typeof argon2$hash
+    hash: typeof argon2$hash,
+
+    ArgonType: typeof argon2$ArgonType
   };
   declare function argon2$hash(
     options: argon2$Argon2BrowserHashOptions
@@ -19,20 +21,12 @@ declare module "argon2-browser" {
 
   declare interface argon2$Argon2BrowserHashResult {
     encoded: string;
-    argon2$hash: Uint8Array;
+    hash: Uint8Array;
     hashHex: string;
   }
 
-  declare class argon2$ArgonType {
-    constructor(...args: empty): mixed;
-    static +Argon2d: Class<argon2$ArgonType__Argon2d> &
-      argon2$ArgonType__Argon2d &
-      0; // 0
-    static +Argon2i: Class<argon2$ArgonType__Argon2i> &
-      argon2$ArgonType__Argon2i &
-      1; // 1
-  }
-
-  declare class argon2$ArgonType__Argon2d mixins argon2$ArgonType {}
-  declare class argon2$ArgonType__Argon2i mixins argon2$ArgonType {}
+  declare var argon2$ArgonType: {|
+    +Argon2d: 0, // 0
+    +Argon2i: 1 // 1
+  |};
 }
