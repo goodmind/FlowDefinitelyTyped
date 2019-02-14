2f204c98846d3e61b4b938d1e7e65ee1a787403e
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:28:31 +0500

    [google.picker] update types

--- flow-types/types/google.picker_vx.x.x/flow_v0.25.x-/google.picker.js
+++ flow-types/types/google.picker_vx.x.x/flow_v0.25.x-/google.picker.js
@@ -1,36 +1,44 @@
 declare module "google.picker" {
-  declare var npm$namespace$picker: {
-    DocsViewMode: typeof picker$DocsViewMode,
-    Feature: typeof picker$Feature,
-    ViewId: typeof picker$ViewId,
-    Action: typeof picker$Action,
-    Document: typeof picker$Document,
-    Response: typeof picker$Response,
-    Type: typeof picker$Type
+  declare var npm$namespace$google: {
+    picker: typeof npm$namespace$google$picker
   };
-  declare export class picker$PickerBuilder {
+
+  declare var npm$namespace$google$picker: {
+    DocsViewMode: typeof google$picker$DocsViewMode,
+    Feature: typeof google$picker$Feature,
+    ViewId: typeof google$picker$ViewId,
+    Action: typeof google$picker$Action,
+    Document: typeof google$picker$Document,
+    Response: typeof google$picker$Response,
+    Type: typeof google$picker$Type,
+
+    PickerBuilder: typeof google$picker$PickerBuilder,
+    DocsUploadView: typeof google$picker$DocsUploadView,
+    DocsView: typeof google$picker$DocsView
+  };
+  declare export class google$picker$PickerBuilder {
     constructor(): this;
-    addView(viewOrId: any): picker$PickerBuilder;
-    addViewGroup(viewGroup: any): picker$PickerBuilder;
-    disableFeature(feature: string): picker$PickerBuilder;
-    enableFeature(feature: string): picker$PickerBuilder;
+    addView(viewOrId: any): google$picker$PickerBuilder;
+    addViewGroup(viewGroup: any): google$picker$PickerBuilder;
+    disableFeature(feature: string): google$picker$PickerBuilder;
+    enableFeature(feature: string): google$picker$PickerBuilder;
     getRelayUrl(): string;
     getTitle(): string;
-    hideTitleBar(): picker$PickerBuilder;
+    hideTitleBar(): google$picker$PickerBuilder;
     isFeatureEnabled(feature: string): boolean;
-    setAppId(appId: string): picker$PickerBuilder;
-    setCallback(method: Function): picker$PickerBuilder;
-    setDeveloperKey(key: string): picker$PickerBuilder;
-    setDocument(document: string): picker$PickerBuilder;
-    setLocale(locale: string): picker$PickerBuilder;
-    setMaxItems(max: number): picker$PickerBuilder;
-    setOAuthToken(token: string): picker$PickerBuilder;
-    setOrigin(origin: string): picker$PickerBuilder;
-    setRelayUrl(url: string): picker$PickerBuilder;
-    setSelectableMimeTypes(type: string): picker$PickerBuilder;
-    setSize(width: number, height: number): picker$PickerBuilder;
-    setTitle(title: string): picker$PickerBuilder;
-    setUploadToAlbumId(albumId: string): picker$PickerBuilder;
+    setAppId(appId: string): google$picker$PickerBuilder;
+    setCallback(method: Function): google$picker$PickerBuilder;
+    setDeveloperKey(key: string): google$picker$PickerBuilder;
+    setDocument(document: string): google$picker$PickerBuilder;
+    setLocale(locale: string): google$picker$PickerBuilder;
+    setMaxItems(max: number): google$picker$PickerBuilder;
+    setOAuthToken(token: string): google$picker$PickerBuilder;
+    setOrigin(origin: string): google$picker$PickerBuilder;
+    setRelayUrl(url: string): google$picker$PickerBuilder;
+    setSelectableMimeTypes(type: string): google$picker$PickerBuilder;
+    setSize(width: number, height: number): google$picker$PickerBuilder;
+    setTitle(title: string): google$picker$PickerBuilder;
+    setUploadToAlbumId(albumId: string): google$picker$PickerBuilder;
     toUri(): string;
     build(): picker$Picker;
   }
@@ -38,45 +46,45 @@ declare module "google.picker" {
   /**
    * Picker is the top level object representing the UI action with the user. These objects are not created directly, but instead use the PickerBuilder object.
    */
-  declare export interface picker$Picker {
+  declare export interface google$picker$Picker {
     isVisible(): boolean;
-    setCallback(method: Function): picker$Picker;
-    setRelayUrl(url: string): picker$Picker;
-    setVisible(visible: boolean): picker$Picker;
+    setCallback(method: Function): google$picker$Picker;
+    setRelayUrl(url: string): google$picker$Picker;
+    setVisible(visible: boolean): google$picker$Picker;
   }
 
   /**
    * Use DocsUploadView to upload documents to Google Drive.
    */
-  declare export class picker$DocsUploadView {
+  declare export class google$picker$DocsUploadView {
     constructor(): this;
-    setIncludeFolders(included: boolean): picker$DocsUploadView;
-    setParent(parentId: string): picker$DocsUploadView;
+    setIncludeFolders(included: boolean): google$picker$DocsUploadView;
+    setParent(parentId: string): google$picker$DocsUploadView;
   }
 
   /**
    * DocsView is a subclass of View that can be used for Google Drive views.
    */
-  declare export class picker$DocsView {
+  declare export class google$picker$DocsView {
     constructor(viewId?: string): this;
-    setIncludeFolders(included: boolean): picker$DocsView;
-    setSelectFolderEnabled(enabled: boolean): picker$DocsView;
-    setMode(mode: string): picker$DocsView;
-    setOwnedByMe(me?: boolean): picker$DocsView;
-    setParent(parentId: string): picker$DocsView;
-    setStarred(starred: boolean): picker$DocsView;
-    setEnableTeamDrives(enabled: boolean): picker$DocsView;
+    setIncludeFolders(included: boolean): google$picker$DocsView;
+    setSelectFolderEnabled(enabled: boolean): google$picker$DocsView;
+    setMode(mode: string): google$picker$DocsView;
+    setOwnedByMe(me?: boolean): google$picker$DocsView;
+    setParent(parentId: string): google$picker$DocsView;
+    setStarred(starred: boolean): google$picker$DocsView;
+    setEnableTeamDrives(enabled: boolean): google$picker$DocsView;
   }
 
   /**
    * DocsViewMode is an enumerated type for displaying data within a DocsView. Use these values in calls to DocsView.setMode.
    */
-  declare export var picker$DocsViewMode: {
+  declare export var google$picker$DocsViewMode: {
     GRID: string,
     LIST: string
   };
 
-  declare export var picker$Feature: {
+  declare export var google$picker$Feature: {
     MINE_ONLY: string,
     MULTISELECT_ENABLED: string,
     NAV_HIDDEN: string,
@@ -84,7 +92,7 @@ declare module "google.picker" {
     SUPPORT_TEAM_DRIVES: string
   };
 
-  declare export var picker$ViewId: {
+  declare export var google$picker$ViewId: {
     DOCS: string,
     DOCS_IMAGES: string,
     DOCS_IMAGES_AND_VIDEOS: string,
@@ -107,7 +115,7 @@ declare module "google.picker" {
     YOUTUBE: string
   };
 
-  declare export var picker$Action: {
+  declare export var google$picker$Action: {
     CANCEL: string,
     PICKED: string
   };
@@ -115,7 +123,7 @@ declare module "google.picker" {
   /**
    * Document is an enumerated type used to convey information about a specific picked item. Only fields which are relevant to the selected item are returned. This value will be in the Response.DOCUMENTS field in the callback data.
    */
-  declare export var picker$Document: {
+  declare export var google$picker$Document: {
     ADDRESS_LINES: string,
     AUDIENCE: string,
     DESCRIPTION: string,
@@ -141,14 +149,14 @@ declare module "google.picker" {
   /**
    * Response is an enumerated type used to convey information about the user's picked items.
    */
-  declare export var picker$Response: {
+  declare export var google$picker$Response: {
     ACTION: string,
     DOCUMENTS: string,
     PARENTS: string,
     VIEW: string
   };
 
-  declare export var picker$Type: {
+  declare export var google$picker$Type: {
     ALBUM: string,
     DOCUMENT: string,
     LOCATION: string,
