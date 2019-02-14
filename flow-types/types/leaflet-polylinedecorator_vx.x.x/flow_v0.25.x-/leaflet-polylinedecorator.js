7a90a245650be8bb32b29d11753ea0de82c80b68
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:38:49 +0500

    [leaflet-polylinedecorator] update types

--- flow-types/types/leaflet-polylinedecorator_vx.x.x/flow_v0.25.x-/leaflet-polylinedecorator.js
+++ flow-types/types/leaflet-polylinedecorator_vx.x.x/flow_v0.25.x-/leaflet-polylinedecorator.js
@@ -1,50 +1,55 @@
-declare var npm$namespace$Symbol: {
-  dash: typeof Symbol$dash,
-  arrowHead: typeof Symbol$arrowHead,
-  marker: typeof Symbol$marker
-};
-declare interface Symbol$DashOptions {
-  pixelSize?: number;
-  pathOptions?: PathOptions;
-}
-
-declare class Symbol$Dash {
-  constructor(options?: Symbol$DashOptions): this;
-}
-
-declare function Symbol$dash(options?: Symbol$DashOptions): Symbol$Dash;
-
-declare interface Symbol$ArrowHeadOptions {
-  polygon?: boolean;
-  pixelSize?: number;
-  headAngle?: number;
-  pathOptions?: PathOptions;
-}
-
-declare class Symbol$ArrowHead {
-  constructor(options?: Symbol$ArrowHeadOptions): this;
-}
-
-declare function Symbol$arrowHead(
-  options?: Symbol$ArrowHeadOptions
-): Symbol$ArrowHead;
-
-declare interface Symbol$MarkerOptions {
-  rotate?: boolean;
-  markerOptions?: L.Symbol$MarkerOptions;
-}
-
-declare class Symbol$Marker {
-  constructor(options?: Symbol$MarkerOptions): this;
-}
-
-declare function Symbol$marker(options?: Symbol$MarkerOptions): Symbol$Marker;
 declare module "leaflet" {
+  declare var npm$namespace$Symbol: {
+    dash: typeof Symbol$dash,
+    arrowHead: typeof Symbol$arrowHead,
+    marker: typeof Symbol$marker,
+
+    Dash: typeof Symbol$Dash,
+    ArrowHead: typeof Symbol$ArrowHead,
+    Marker: typeof Symbol$Marker
+  };
+  declare interface Symbol$DashOptions {
+    pixelSize?: number;
+    pathOptions?: PathOptions;
+  }
+
+  declare class Symbol$Dash {
+    constructor(options?: Symbol$DashOptions): this;
+  }
+
+  declare function Symbol$dash(options?: Symbol$DashOptions): Symbol$Dash;
+
+  declare interface Symbol$ArrowHeadOptions {
+    polygon?: boolean;
+    pixelSize?: number;
+    headAngle?: number;
+    pathOptions?: PathOptions;
+  }
+
+  declare class Symbol$ArrowHead {
+    constructor(options?: Symbol$ArrowHeadOptions): this;
+  }
+
+  declare function Symbol$arrowHead(
+    options?: Symbol$ArrowHeadOptions
+  ): Symbol$ArrowHead;
+
+  declare interface Symbol$MarkerOptions {
+    rotate?: boolean;
+    markerOptions?: L.MarkerOptions;
+  }
+
+  declare class Symbol$Marker {
+    constructor(options?: Symbol$MarkerOptions): this;
+  }
+
+  declare function Symbol$marker(options?: Symbol$MarkerOptions): Symbol$Marker;
+
   declare interface Pattern {
     offset?: number | string;
     endOffset?: number | string;
     repeat: number | string;
-    symbol: Symbol$Symbol$Dash | Symbol$Symbol$ArrowHead | Symbol$Symbol$Marker;
+    symbol: Symbol$Dash | Symbol$ArrowHead | Symbol$Marker;
   }
   declare interface PolylineDecoratorOptions {
     patterns: Pattern[];
