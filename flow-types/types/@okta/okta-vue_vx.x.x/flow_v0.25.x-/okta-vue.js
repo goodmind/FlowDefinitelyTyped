4453dc9c34c5eb9285443bc081fdb532d2e9bba5
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:04:28 +0500

    [@okta/okta-vue] update types

--- flow-types/types/@okta/okta-vue_vx.x.x/flow_v0.25.x-/okta-vue.js
+++ flow-types/types/@okta/okta-vue_vx.x.x/flow_v0.25.x-/okta-vue.js
@@ -3,7 +3,7 @@ declare module "vue/types/vue" {
     $auth: {
       loginRedirect(
         fromUri?: string,
-        additionalParams?: OktaVuePlugin$OktaVuePlugin$OktaOpenIDOptions
+        additionalParams?: OktaVuePlugin$OktaOpenIDOptions
       ): void,
       logout(): Promise<void>,
       isAuthenticated(): Promise<boolean>,
@@ -16,38 +16,38 @@ declare module "vue/types/vue" {
     };
   }
 }
-
-declare var npm$namespace$OktaVuePlugin: {
-  install: typeof OktaVuePlugin$install,
-  handleCallback: typeof OktaVuePlugin$handleCallback
-};
-declare interface OktaVuePlugin$OktaVueOptions {
-  issuer: string;
-  client_id: string;
-  redirect_uri: string;
-  scope?: string;
-  response_type?: string;
-}
-
-declare interface OktaVuePlugin$OktaOpenIDOptions {
-  sessionToken?: string;
-  responseMode?: string;
-  responseType?: string | string[];
-  scopes?: string[];
-  state?: string;
-  nonce?: string;
-}
-
-declare function OktaVuePlugin$install(
-  vm: VueConstructor,
-  options: OktaVuePlugin$OktaVueOptions
-): PluginFunction<VueConstructor>;
-
-declare function OktaVuePlugin$handleCallback(): VueConstructor;
 declare module "@okta/okta-vue" {
   import type { VueConstructor, PluginFunction } from "vue";
 
   import type { NavigationGuard } from "vue-router";
+
+  declare var npm$namespace$OktaVuePlugin: {
+    install: typeof OktaVuePlugin$install,
+    handleCallback: typeof OktaVuePlugin$handleCallback
+  };
+  declare interface OktaVuePlugin$OktaVueOptions {
+    issuer: string;
+    client_id: string;
+    redirect_uri: string;
+    scope?: string;
+    response_type?: string;
+  }
+
+  declare interface OktaVuePlugin$OktaOpenIDOptions {
+    sessionToken?: string;
+    responseMode?: string;
+    responseType?: string | string[];
+    scopes?: string[];
+    state?: string;
+    nonce?: string;
+  }
+
+  declare function OktaVuePlugin$install(
+    vm: VueConstructor,
+    options: OktaVuePlugin$OktaVueOptions
+  ): PluginFunction<VueConstructor>;
+
+  declare function OktaVuePlugin$handleCallback(): VueConstructor;
 
   declare function OktaVuePlugin(): PluginFunction<VueConstructor>;
 
