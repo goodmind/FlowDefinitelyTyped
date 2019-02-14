86d7957d7ee00ae3ce84604771fc61b0e9d0ba79
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:52:56 +0500

    [proj4leaflet] update types

--- flow-types/types/proj4leaflet_vx.x.x/flow_v0.25.x-/proj4leaflet.js
+++ flow-types/types/proj4leaflet_vx.x.x/flow_v0.25.x-/proj4leaflet.js
@@ -1,57 +1,61 @@
-declare var npm$namespace$Proj: {
-  geoJson: typeof Proj$geoJson,
-  imageOverlay: typeof Proj$imageOverlay
-};
-declare class Proj$CRS mixins Proj$CRS {
-  projection: Projection;
-  transformation: Transformation;
-  code: string;
-  wrapLng: [number, number];
-  wrapLat: [number, number];
-  infinite: boolean;
-  constructor(
-    projection: proj4.InterfaceProjection,
-    options?: Proj$ProjCRSOptions
-  ): this;
-  constructor(
-    code: string,
-    proj4def: string,
-    options?: Proj$ProjCRSOptions
-  ): this;
-  latLngToPoint(latlng: LatLngExpression, zoom: number): Point;
-  pointToLatLng(point: PointExpression, zoom: number): LatLng;
-  project(latlng: LatLng | LatLngLiteral): Point;
-  unproject(point: PointExpression): LatLng;
-  scale(zoom: number): number;
-  zoom(scale: number): number;
-  getProjectedBounds(zoom: number): Bounds;
-  distance(latlng1: LatLngExpression, latlng2: LatLngExpression): number;
-  wrapLatLng(latlng: LatLng | LatLngLiteral): LatLng;
-}
-
-declare class Proj$GeoJSON mixins L.Proj$GeoJSON {}
-
-declare var Proj$geoJson: (
-  geojson?: Proj4GeoJSONFeature,
-  options?: GeoJSONOptions
-) => Proj$GeoJSON;
-
-declare class Proj$ImageOverlay mixins L.Proj$ImageOverlay {}
-
-declare var Proj$imageOverlay: (
-  imageUrl: string,
-  bounds: LatLngBoundsExpression,
-  options?: ImageOverlayOptions
-) => Proj$ImageOverlay;
-
-declare interface Proj$ProjCRSOptions {
-  bounds?: Bounds;
-  origin?: [number, number];
-  scales?: number[];
-  resolutions?: number[];
-  transformation?: Transformation;
-}
 declare module "leaflet" {
+  declare var npm$namespace$Proj: {
+    geoJson: typeof Proj$geoJson,
+    imageOverlay: typeof Proj$imageOverlay,
+
+    CRS: typeof Proj$CRS,
+    GeoJSON: typeof Proj$GeoJSON,
+    ImageOverlay: typeof Proj$ImageOverlay
+  };
+  declare class Proj$CRS mixins CRS {
+    projection: Projection;
+    transformation: Transformation;
+    code: string;
+    wrapLng: [number, number];
+    wrapLat: [number, number];
+    infinite: boolean;
+    constructor(
+      projection: proj4.InterfaceProjection,
+      options?: Proj$ProjCRSOptions
+    ): this;
+    constructor(
+      code: string,
+      proj4def: string,
+      options?: Proj$ProjCRSOptions
+    ): this;
+    latLngToPoint(latlng: LatLngExpression, zoom: number): Point;
+    pointToLatLng(point: PointExpression, zoom: number): LatLng;
+    project(latlng: LatLng | LatLngLiteral): Point;
+    unproject(point: PointExpression): LatLng;
+    scale(zoom: number): number;
+    zoom(scale: number): number;
+    getProjectedBounds(zoom: number): Bounds;
+    distance(latlng1: LatLngExpression, latlng2: LatLngExpression): number;
+    wrapLatLng(latlng: LatLng | LatLngLiteral): LatLng;
+  }
+
+  declare class Proj$GeoJSON mixins L.GeoJSON {}
+
+  declare var Proj$geoJson: (
+    geojson?: Proj4GeoJSONFeature,
+    options?: GeoJSONOptions
+  ) => Proj$GeoJSON;
+
+  declare class Proj$ImageOverlay mixins L.ImageOverlay {}
+
+  declare var Proj$imageOverlay: (
+    imageUrl: string,
+    bounds: LatLngBoundsExpression,
+    options?: ImageOverlayOptions
+  ) => Proj$ImageOverlay;
+
+  declare interface Proj$ProjCRSOptions {
+    bounds?: Bounds;
+    origin?: [number, number];
+    scales?: number[];
+    resolutions?: number[];
+    transformation?: Transformation;
+  }
 }
 declare module "proj4leaflet" {
   import typeof * as geojson from "geojson";
