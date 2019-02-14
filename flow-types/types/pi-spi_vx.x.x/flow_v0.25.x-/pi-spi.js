181c4c47a6569bc82e142d105aad54678e34a7d2
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:51:47 +0500

    [pi-spi] update types

--- flow-types/types/pi-spi_vx.x.x/flow_v0.25.x-/pi-spi.js
+++ flow-types/types/pi-spi_vx.x.x/flow_v0.25.x-/pi-spi.js
@@ -1,40 +1,33 @@
 declare module "pi-spi" {
-  declare module.exports: typeof __PI_SPI;
+  declare export default typeof __PI_SPI;
 
   declare var npm$namespace$__PI_SPI: {
-    initialize: typeof __PI_SPI$initialize
+    initialize: typeof __PI_SPI$initialize,
+
+    mode: typeof __PI_SPI$mode,
+    order: typeof __PI_SPI$order,
+    SPI: typeof __PI_SPI$SPI
   };
-  declare class __PI_SPI$mode {
-    constructor(...args: empty): mixed;
-    static +CPHA: Class<__PI_SPI$mode__CPHA> & __PI_SPI$mode__CPHA & 1; // 1
-    static +CPOL: Class<__PI_SPI$mode__CPOL> & __PI_SPI$mode__CPOL & 2; // 2
-  }
 
-  declare class __PI_SPI$mode__CPHA mixins __PI_SPI$mode {}
-  declare class __PI_SPI$mode__CPOL mixins __PI_SPI$mode {}
+  declare var __PI_SPI$mode: {|
+    +CPHA: 1, // 1
+    +CPOL: 2 // 2
+  |};
 
-  declare class __PI_SPI$order {
-    constructor(...args: empty): mixed;
-    static +MSB_FIRST: Class<__PI_SPI$order__MSB_FIRST> &
-      __PI_SPI$order__MSB_FIRST &
-      0; // 0
-    static +LSB_FIRST: Class<__PI_SPI$order__LSB_FIRST> &
-      __PI_SPI$order__LSB_FIRST &
-      1; // 1
-  }
+  declare var __PI_SPI$order: {|
+    +MSB_FIRST: 0, // 0
+    +LSB_FIRST: 1 // 1
+  |};
 
-  declare class __PI_SPI$order__MSB_FIRST mixins __PI_SPI$order {}
-  declare class __PI_SPI$order__LSB_FIRST mixins __PI_SPI$order {}
-
-  declare function __PI_SPI$initialize(device: string): __PI_SPI$__PI_SPI$SPI;
+  declare function __PI_SPI$initialize(device: string): __PI_SPI$SPI;
 
   declare class __PI_SPI$SPI {
     clockSpeed(): number;
     clockSpeed(speed: number): void;
     dataMode(): number;
-    dataMode(__PI_SPI$mode: __PI_SPI$mode): void;
+    dataMode(mode: __PI_SPI$mode): void;
     bitOrder(): number;
-    bitOrder(__PI_SPI$order: __PI_SPI$order): void;
+    bitOrder(order: __PI_SPI$order): void;
     write(writebuf: Buffer, cb: (error: Error, data: Buffer) => void): void;
     read(readcount: number, cb: (error: Error, data: Buffer) => void): void;
     transfer(writebuf: Buffer, cb: (error: Error, data: Buffer) => void): void;
