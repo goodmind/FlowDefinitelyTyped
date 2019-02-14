d737919f4ad1ef15a398adc1561005c7927dbb26
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:03:42 +0500

    [@iopipe/iopipe] update types

--- flow-types/types/@iopipe/iopipe_vx.x.x/flow_v0.25.x-/iopipe.js
+++ flow-types/types/@iopipe/iopipe_vx.x.x/flow_v0.25.x-/iopipe.js
@@ -1,23 +1,25 @@
 declare module "@iopipe/iopipe" {
   declare function iopipe(
-    config?: iopipe$iopipe$LibraryConfig
-  ): iopipe$iopipe$FunctionWrapper;
+    config?: iopipe$LibraryConfig
+  ): iopipe$FunctionWrapper;
 
   declare var npm$namespace$iopipe: {
     label: typeof iopipe$label,
-    metric: typeof iopipe$metric
+    metric: typeof iopipe$metric,
+
+    mark: typeof npm$namespace$iopipe$mark
   };
-  declare function iopipe$label(iopipe$label: string): void;
+  declare function iopipe$label(label: string): void;
 
-  declare function iopipe$metric(iopipe$label: string, value: number): void;
+  declare function iopipe$metric(label: string, value: number): void;
 
-  declare var npm$namespace$mark: {
-    start: typeof mark$start,
-    end: typeof mark$end
+  declare var npm$namespace$iopipe$mark: {
+    start: typeof iopipe$mark$start,
+    end: typeof iopipe$mark$end
   };
-  declare function mark$start(iopipe$label: string): void;
+  declare function iopipe$mark$start(label: string): void;
 
-  declare function mark$end(iopipe$label: string): void;
+  declare function iopipe$mark$end(label: string): void;
 
   declare interface iopipe$LibraryConfig {
     debug?: boolean;
@@ -27,5 +29,5 @@ declare module "@iopipe/iopipe" {
   }
 
   declare type iopipe$FunctionWrapper = <T>(handler: T) => T;
-  declare module.exports: typeof iopipe;
+  declare export default typeof iopipe;
 }
