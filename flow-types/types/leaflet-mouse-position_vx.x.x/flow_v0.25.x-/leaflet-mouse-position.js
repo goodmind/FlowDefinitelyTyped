6264eaf951f44ae473822a3bdddf5bc0660af6ee
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:38:49 +0500

    [leaflet-mouse-position] update types

--- flow-types/types/leaflet-mouse-position_vx.x.x/flow_v0.25.x-/leaflet-mouse-position.js
+++ flow-types/types/leaflet-mouse-position_vx.x.x/flow_v0.25.x-/leaflet-mouse-position.js
@@ -1,26 +1,31 @@
-declare type Control$MousePositionControlOptions = {
-  separator?: string,
-  emptyString?: string,
-  lngFirst?: boolean,
-  numDigits?: number,
-  lngFormatter?: (lng: number) => string,
-  latFormatter?: (lat: number) => string,
-  prefix?: string
-} & ControlOptions;
-
-declare class Control$MousePosition mixins LControl {
-  options: Control$MousePositionControlOptions;
-  constructor(options?: Control$MousePositionControlOptions): this;
-}
-declare var npm$namespace$control: {
-  mousePosition: typeof control$mousePosition
-};
-declare function control$mousePosition(
-  options?: Control$Control$MousePositionControlOptions
-): Control$Control$MousePosition;
 declare module "leaflet" {
   declare interface MapOptions {
     positionControl?: boolean;
+  }
+
+  declare var npm$namespace$control: {
+    mousePosition: typeof control$mousePosition
+  };
+  declare function control$mousePosition(
+    options?: Control$MousePositionControlOptions
+  ): Control$MousePosition;
+
+  declare var npm$namespace$Control: {
+    MousePosition: typeof Control$MousePosition
+  };
+  declare type Control$MousePositionControlOptions = {
+    separator?: string,
+    emptyString?: string,
+    lngFirst?: boolean,
+    numDigits?: number,
+    lngFormatter?: (lng: number) => string,
+    latFormatter?: (lat: number) => string,
+    prefix?: string
+  } & ControlOptions;
+
+  declare class Control$MousePosition mixins LControl {
+    options: Control$MousePositionControlOptions;
+    constructor(options?: Control$MousePositionControlOptions): this;
   }
 }
 declare module "leaflet-mouse-position" {
