de620737041c6b45a4915fe977a14d57d2253032
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:50:28 +0500

    [parse-git-config] update types

--- flow-types/types/parse-git-config_vx.x.x/flow_v0.25.x-/parse-git-config.js
+++ flow-types/types/parse-git-config_vx.x.x/flow_v0.25.x-/parse-git-config.js
@@ -1,34 +1,34 @@
 declare module "parse-git-config" {
-  declare module.exports: typeof parse;
+  declare export default typeof parse;
 
   /**
    * Asynchronously parse a `.git/config` file. If only the callback is passed,
    * the `.git/config` file relative to `process.cwd()` is used.
    * @example ```js
-   * parse((err, config) => {
-   *   if (err) throw err;
-   *   // do stuff with config
-   * });
-   *
-   * // or, using async/await
-   * (async () => {
-   *   console.log(await parse());
-   *   console.log(await parse({ cwd: 'foo' }));
-   *   console.log(await parse({ cwd: 'foo', path: 'some/.git/config' }));
-   * })();
-   * ```
+   *  * parse((err, config) => {
+   *  *   if (err) throw err;
+   *  *   // do stuff with config
+   *  * });
+   *  *
+   *  * // or, using async/await
+   *  * (async () => {
+   *  *   console.log(await parse());
+   *  *   console.log(await parse({ cwd: 'foo' }));
+   *  *   console.log(await parse({ cwd: 'foo', path: 'some/.git/config' }));
+   *  * })();
+   *  * ```
    * @param options Options with `cwd` or `path`, the cwd to use, or the callback function.
    * @param callback callback function if the first argument is options or cwd.
    */
-  declare function parse(cb: parse$parse$ParseCallback): void;
+  declare function parse(cb: parse$ParseCallback): void;
 
   declare function parse(
-    options?: parse$parse$Options | string
-  ): Promise<parse$parse$Config | null>;
+    options?: parse$Options | string
+  ): Promise<parse$Config | null>;
 
   declare function parse(
-    options: parse$parse$Options | string,
-    cb: parse$parse$ParseCallback
+    options: parse$Options | string,
+    cb: parse$ParseCallback
   ): void;
 
   declare var npm$namespace$parse: {
@@ -51,10 +51,10 @@ declare module "parse-git-config" {
    * Synchronously parse a `.git/config` file. If no arguments are passed,
    * the `.git/config` file relative to `process.cwd()` is used.
    * @example ```js
-   * console.log(parse.sync());
-   * console.log(parse.sync({ cwd: 'foo' }));
-   * console.log(parse.sync({ cwd: 'foo', path: 'some/.git/config' }));
-   * ```
+   *      * console.log(parse.sync());
+   *      * console.log(parse.sync({ cwd: 'foo' }));
+   *      * console.log(parse.sync({ cwd: 'foo', path: 'some/.git/config' }));
+   *      * ```
    * @param options Options with `cwd` or `path`, or the cwd to use.
    */
   declare function parse$sync(options?: parse$Options | string): parse$Config;
@@ -63,9 +63,9 @@ declare module "parse-git-config" {
    * Returns an object with only the properties that had ini-style keys
    * converted to objects.
    * @example ```js
-   * const config = parse.sync({ path: '/path/to/.gitconfig' });
-   * const obj = parse.expandKeys(config);
-   * ```
+   *      * const config = parse.sync({ path: '/path/to/.gitconfig' });
+   *      * const obj = parse.expandKeys(config);
+   *      * ```
    * @param config The parsed git config object.
    */
   declare function parse$expandKeys(config: parse$Config): parse$Config;
@@ -85,8 +85,8 @@ declare module "parse-git-config" {
 
   declare type parse$Options = {
     include?: boolean,
-    parse$expandKeys?: boolean
-  } & parse$ResolveConfigOptions;
+    expandKeys?: boolean
+  } & ResolveConfigOptions;
 
   declare type parse$ParseCallback = (
     err: Error | null,
