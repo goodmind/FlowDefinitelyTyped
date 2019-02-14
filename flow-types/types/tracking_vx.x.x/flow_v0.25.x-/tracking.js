0fdc903ebd231932774bf37bb540cd43bda36980
andretshurotshka <andwebar@gmail.com>
2019-02-14 13:07:48 +0500

    [tracking] update types

--- flow-types/types/tracking_vx.x.x/flow_v0.25.x-/tracking.js
+++ flow-types/types/tracking_vx.x.x/flow_v0.25.x-/tracking.js
@@ -1,6 +1,12 @@
 declare module "tracking" {
   declare var npm$namespace$tracking: {
-    track: typeof tracking$track
+    track: typeof tracking$track,
+
+    ColorTracker: typeof tracking$ColorTracker,
+    ObjectTracker: typeof tracking$ObjectTracker,
+    Tracker: typeof tracking$Tracker,
+    EventEmitter: typeof tracking$EventEmitter,
+    TrackerTask: typeof tracking$TrackerTask
   };
 
   /**
@@ -23,7 +29,7 @@ declare module "tracking" {
    * ColorTracker utility to track colored blobs in a frame using color
    * difference evaluation.
    */
-  declare class tracking$ColorTracker mixins tracking$Tracker {
+  declare class tracking$ColorTracker mixins Tracker {
     /**
      * Create a new ColorTracker.
      * @param colors Optional color(s) to track.
@@ -31,11 +37,11 @@ declare module "tracking" {
     constructor(colors?: string | string[]): this;
 
     /**
- * Registers a color as known color.
- * @param name The color name.
- * @param predicate The color function to test if the passed (r,g,b) is
-the desired color.
- */
+     * Registers a color as known color.
+     * @param name The color name.
+     * @param predicate The color function to test if the passed (r,g,b) is
+     * the desired color.
+     */
     static registerColor(name: string, predicate: tracking$ColorFunction): void;
 
     /**
@@ -100,7 +106,7 @@ the desired color.
   /**
    * Object tracker utility.
    */
-  declare class tracking$ObjectTracker mixins tracking$Tracker {
+  declare class tracking$ObjectTracker mixins Tracker {
     /**
      * Create a new Object Tracker.
      * @param classifiers Optional object classifiers to track.
@@ -173,7 +179,7 @@ the desired color.
   /**
    * Base class for specific tracker types.
    */
-  declare class tracking$Tracker mixins tracking$EventEmitter {
+  declare class tracking$Tracker mixins EventEmitter {
     /**
      * Tracks the pixels on the array. This method is called for each video
      * frame in order to emit `track` event.
@@ -210,13 +216,13 @@ the desired color.
     on(event: string, listener: tracking$TrackEventListener): this;
 
     /**
- * Adds a one time listener for the event. This listener is invoked only the
- * next time the event is fired, after which it is removed.
- * @param event Name of the event.
- * @param listener Listener function.
-FIXME: tracking.js docs says method should return this, but code does not
-this should be updated when fixed in the repo.
- */
+     * Adds a one time listener for the event. This listener is invoked only the
+     * next time the event is fired, after which it is removed.
+     * @param event Name of the event.
+     * @param listener Listener function.
+     * FIXME: tracking.js docs says method should return this, but code does not
+     * this should be updated when fixed in the repo.
+     */
     once(event: string, listener: tracking$TrackEventListener): void;
 
     /**
@@ -241,7 +247,7 @@ this should be updated when fixed in the repo.
    * Note: Types do not provide `setTracker()` even though not marked as private.
    * A new tracker should be created instead.
    */
-  declare class tracking$TrackerTask mixins tracking$EventEmitter {
+  declare class tracking$TrackerTask mixins EventEmitter {
     /**
      * Create a new tracker task.
      * @param tracker The tracker instance to manage.
@@ -304,19 +310,19 @@ this should be updated when fixed in the repo.
   }
 
   /**
- * Tracks a canvas, image or video element based on the specified `tracker`
- * instance. This method extract the pixel information of the input element
- * to pass to the `tracker` instance. When tracking a video, the
- * `tracker.track(pixels, width, height)` will be in a
- * `requestAnimationFrame` loop in order to track all video frames.
- * @param element HTMLElement or CSS3 selector string for the element to track.
-Element must be convas, image, or video.
- * @param tracker Tracker instance used to track the element.
- * @param options Optional tracker configuration.
- */
+   * Tracks a canvas, image or video element based on the specified `tracker`
+   * instance. This method extract the pixel information of the input element
+   * to pass to the `tracker` instance. When tracking a video, the
+   * `tracker.track(pixels, width, height)` will be in a
+   * `requestAnimationFrame` loop in order to track all video frames.
+   * @param element HTMLElement or CSS3 selector string for the element to track.
+   * Element must be convas, image, or video.
+   * @param tracker Tracker instance used to track the element.
+   * @param options Optional tracker configuration.
+   */
   declare function tracking$track(
     element: HTMLElement | string,
-    tracker: tracking$tracking$Tracker,
+    tracker: tracking$Tracker,
     options?: {
       camera?: boolean,
       audio?: boolean
