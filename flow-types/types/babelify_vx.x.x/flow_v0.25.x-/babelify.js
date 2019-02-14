5f5a05aeb5109c97de717aa2b7fa0b1871542180
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:09:20 +0500

    [babelify] update types

--- flow-types/types/babelify_vx.x.x/flow_v0.25.x-/babelify.js
+++ flow-types/types/babelify_vx.x.x/flow_v0.25.x-/babelify.js
@@ -1,47 +1,51 @@
-declare var npm$namespace$Babelify: {
-  configure: typeof Babelify$configure
-};
-export interface Babelify$BabelifyConstructor {
-  (
-    filename: string,
-    opts: Babelify$Babelify$BabelifyOptions
-  ): Babelify$Babelify$BabelifyObject;
-}
-
-/**
- * In addition to the various purposes documented here, all of the babelify options are passed to babel which passes them on to babel.transform() when each file is transformed
- */
-export type Babelify$BabelifyOptions = {
-  /**
-   * These are passed to babel.util.canCompile() for each filename
-   * default: null
-   */
-  extensions?: string | string[],
-
-  /**
-   * if true, a 'sourceFileName' property with a value equal to the current file being transformed is included with the options passed to babel.transform()
-   * default: false
-   */
-  sourceMapsAbsolute?: boolean
-} & babel.TransformOptions;
-
-declare export class Babelify$BabelifyObject mixins stream.Transform {
-  _transform(
-    buf: string | Buffer,
-    encoding: string,
-    callback: () => void
-  ): void;
-  _flush(callback: () => void): void;
-}
-
-declare export function Babelify$configure(
-  opts: Babelify$Babelify$BabelifyOptions
-): (filename: string) => Babelify$Babelify$BabelifyObject;
 declare module "babelify" {
+  import typeof * as stream from "stream";
+
+  import typeof * as babel from "babel-core";
+
   declare function Babelify(
     filename: string,
-    opts?: Babelify$Babelify$BabelifyOptions
-  ): Babelify$Babelify$BabelifyObject;
+    opts?: Babelify$BabelifyOptions
+  ): Babelify$BabelifyObject;
+
+  declare var npm$namespace$Babelify: {
+    configure: typeof Babelify$configure,
+
+    BabelifyObject: typeof Babelify$BabelifyObject
+  };
+  declare export interface Babelify$BabelifyConstructor {
+    (filename: string, opts: Babelify$BabelifyOptions): Babelify$BabelifyObject;
+  }
+
+  /**
+   * In addition to the various purposes documented here, all of the babelify options are passed to babel which passes them on to babel.transform() when each file is transformed
+   */
+  declare export type Babelify$BabelifyOptions = {
+    /**
+     * These are passed to babel.util.canCompile() for each filename
+     * default: null
+     */
+    extensions?: string | string[],
+
+    /**
+     * if true, a 'sourceFileName' property with a value equal to the current file being transformed is included with the options passed to babel.transform()
+     * default: false
+     */
+    sourceMapsAbsolute?: boolean
+  } & babel.TransformOptions;
+
+  declare export class Babelify$BabelifyObject mixins stream.Transform {
+    _transform(
+      buf: string | Buffer,
+      encoding: string,
+      callback: () => void
+    ): void;
+    _flush(callback: () => void): void;
+  }
+
+  declare export function Babelify$configure(
+    opts: Babelify$BabelifyOptions
+  ): (filename: string) => Babelify$BabelifyObject;
 
   declare export default typeof Babelify;
 }
