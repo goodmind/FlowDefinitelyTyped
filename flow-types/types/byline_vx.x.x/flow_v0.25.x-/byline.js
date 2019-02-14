0ab4b2d1cf7b56217b5e3726e26a01c21da74ecf
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:12:17 +0500

    [byline] update types

--- flow-types/types/byline_vx.x.x/flow_v0.25.x-/byline.js
+++ flow-types/types/byline_vx.x.x/flow_v0.25.x-/byline.js
@@ -1,32 +1,34 @@
-declare var npm$namespace$bl: {
-  createStream: typeof bl$createStream,
-  LineStream: typeof bl$LineStream
-};
-export type bl$LineStreamOptions = {
-  keepEmptyLines?: boolean
-} & stream.TransformOptions;
-
-export type bl$LineStream = {} & stream.Transform;
-
-export type bl$LineStreamCreatable = {
-  new(options?: bl$LineStreamOptions): bl$LineStream
-} & bl$LineStream;
-
-declare export function bl$createStream(): bl$LineStream;
-
-declare export function bl$createStream(
-  stream: NodeJS.ReadableStream,
-  options?: bl$LineStreamOptions
-): bl$LineStream;
-
-declare export var bl$LineStream: bl$LineStreamCreatable;
 declare module "byline" {
-  declare function bl(): bl$bl$LineStream;
+  import typeof * as stream from "stream";
+
+  declare function bl(): bl$LineStream;
 
   declare function bl(
-    stream: NodeJS.ReadableStream,
-    options?: bl$bl$LineStreamOptions
-  ): bl$bl$LineStream;
+    stream: NodeJS$ReadableStream,
+    options?: bl$LineStreamOptions
+  ): bl$LineStream;
 
+  declare var npm$namespace$bl: {
+    createStream: typeof bl$createStream,
+    LineStream: typeof bl$LineStream
+  };
+  declare export type bl$LineStreamOptions = {
+    keepEmptyLines?: boolean
+  } & stream.TransformOptions;
+
+  declare export type bl$LineStream = {} & stream.Transform;
+
+  declare export type bl$LineStreamCreatable = {
+    new(options?: bl$LineStreamOptions): bl$LineStream
+  } & LineStream;
+
+  declare export function bl$createStream(): bl$LineStream;
+
+  declare export function bl$createStream(
+    stream: NodeJS$ReadableStream,
+    options?: bl$LineStreamOptions
+  ): bl$LineStream;
+
+  declare export var bl$LineStream: bl$LineStreamCreatable;
   declare export default typeof bl;
 }
