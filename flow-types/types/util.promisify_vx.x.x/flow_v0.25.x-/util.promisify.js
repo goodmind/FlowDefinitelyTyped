bcdf8300182840d23ad7999501c9bb4f778a9736
andretshurotshka <andwebar@gmail.com>
2019-02-14 13:09:16 +0500

    [util.promisify] update types

---
+++ flow-types/types/util.promisify_vx.x.x/flow_v0.25.x-/util.promisify.js
@@ -0,0 +1,36 @@
+declare module "util" {
+  declare var promisify: {
+    (fn: (...args: any[]) => void): (...args: any[]) => Promise<any>,
+    custom: Symbol
+  };
+}
+declare module "util.promisify" {
+  declare export default typeof promisify;
+
+  declare function promisify(
+    f: (...args: any[]) => void
+  ): (...args: any[]) => Promise<any>;
+
+  declare var npm$namespace$promisify: {
+    getPolyfill: typeof promisify$getPolyfill,
+    shim: typeof promisify$shim,
+    custom: typeof promisify$custom,
+    customPromisifyArgs: typeof promisify$customPromisifyArgs,
+    implementation: typeof promisify$implementation
+  };
+  declare interface promisify$implementation {
+    (fn: (...args: any[]) => void): (...args: any[]) => Promise<any>;
+    custom: Symbol;
+    customPromisifyArgs: Symbol | void;
+  }
+
+  declare var promisify$custom: Symbol;
+
+  declare var promisify$customPromisifyArgs: Symbol;
+
+  declare function promisify$getPolyfill(): promisify$implementation;
+
+  declare var promisify$implementation: promisify$implementation;
+
+  declare function promisify$shim(): promisify$implementation;
+}
