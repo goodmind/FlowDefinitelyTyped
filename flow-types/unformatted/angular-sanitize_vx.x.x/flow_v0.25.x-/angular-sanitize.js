8ce44c66e7e9160d1ae831b85e0b14ae70226774
andretshurotshka <andwebar@gmail.com>
2019-02-14 13:12:41 +0500

    [angular-sanitize] update types

--- flow-types/unformatted/angular-sanitize_vx.x.x/flow_v0.25.x-/angular-sanitize.js
+++ flow-types/unformatted/angular-sanitize_vx.x.x/flow_v0.25.x-/angular-sanitize.js
@@ -6,17 +6,13 @@ declare module 'angular' {
 
 /**
  * Finds links in text input and turns them into html links. Supports http/https/ftp/mailto and plain email address links.
- * @param text
- * Input text.
- * @param target
- * ILinkyTargetType Window (_blank|_self|_parent|_top) or named frame to open links in.
- * @param attributes
- * Add custom attributes to the link element.
- * @return
- * Html-linkified and sanitized text.
+ * @param text Input text.
+ * @param target ILinkyTargetType Window (_blank|_self|_parent|_top) or named frame to open links in.
+ * @param attributes Add custom attributes to the link element.
+ * @return Html-linkified and sanitized text.
  * see https://docs.angularjs.org/api/ngSanitize/filter/linky
  */
-declare interface filter$ILinky {
+declare interface sanitize$filter$ILinky {
 (text: string, target?: string, attributes?: {
 [attribute: string]: string
 } | ((url: string) => {
@@ -25,7 +21,7 @@ declare interface filter$ILinky {
 } 
 
 declare interface sanitize$IFilterService {
-(name: "linky"): angular.sanitize.filter.filter$ILinky
+(name: "linky"): angular.sanitize.filter.ILinky
 } 
     }
 declare module 'angular-sanitize' {
