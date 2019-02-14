4881c39d59b598be10c0a8f492974d47380d115d
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:25:58 +0500

    [gapi.client] update types

--- flow-types/types/gapi.client_vx.x.x/flow_v0.25.x-/gapi.client.js
+++ flow-types/types/gapi.client_vx.x.x/flow_v0.25.x-/gapi.client.js
@@ -1,6 +1,9 @@
 declare module "gapi.client" {
   declare var npm$namespace$gapi: {
-    load: typeof gapi$load
+    load: typeof gapi$load,
+
+    client: typeof npm$namespace$gapi$client,
+    auth: typeof npm$namespace$gapi$auth
   };
 
   /**
@@ -8,11 +11,11 @@ declare module "gapi.client" {
    */
   declare function gapi$load(api: string, callback: () => void): void;
 
-  declare var npm$namespace$client: {
-    load: typeof client$load,
-    request: typeof client$request,
-    setApiKey: typeof client$setApiKey,
-    newBatch: typeof client$newBatch
+  declare var npm$namespace$gapi$client: {
+    load: typeof gapi$client$load,
+    request: typeof gapi$client$request,
+    setApiKey: typeof gapi$client$setApiKey,
+    newBatch: typeof gapi$client$newBatch
   };
 
   /**
@@ -21,13 +24,13 @@ declare module "gapi.client" {
    * @param version The version of the API to load
    * @param callback the function that is called once the API interface is loaded
    */
-  declare function client$load(
+  declare function gapi$client$load(
     name: string,
     version: string,
     callback: () => any
   ): void;
 
-  declare function client$load(
+  declare function gapi$client$load(
     name: string,
     version: string
   ): PromiseLike<void>;
@@ -36,7 +39,7 @@ declare module "gapi.client" {
    * Creates a HTTP request for making RESTful requests.
    * An object encapsulating the various arguments for this method.
    */
-  declare function client$request<T>(args: {
+  declare function gapi$client$request<T>(args: {
     /**
      * The URL to handle the request
      */
@@ -67,12 +70,12 @@ declare module "gapi.client" {
    * Sets the API key for the application.
    * @param apiKey The API key to set
    */
-  declare function client$setApiKey(apiKey: string): void;
+  declare function gapi$client$setApiKey(apiKey: string): void;
 
   /**
    * An object containing information about the HTTP response
    */
-  declare interface client$Response<T> {
+  declare interface gapi$client$Response<T> {
     result: T;
     body: string;
     headers?: any[];
@@ -83,36 +86,36 @@ declare module "gapi.client" {
   /**
    * An object encapsulating an HTTP request. This object is not instantiated directly, rather it is returned by gapi.client.request.
    */
-  declare type client$Request<T> = {
+  declare type gapi$client$Request<T> = {
     /**
      * Executes the request and runs the supplied callback on response.
      * @param callback The callback function which executes when the request succeeds or fails.
      */
-    execute(callback: (response: client$Response<T>) => any): void
-  } & PromiseLike<client$Response<T>>;
+    execute(callback: (response: gapi$client$Response<T>) => any): void
+  } & PromiseLike<gapi$client$Response<T>>;
 
-  declare interface client$ResponseMap<T> {
-    [id: string]: client$Response<T>;
+  declare interface gapi$client$ResponseMap<T> {
+    [id: string]: gapi$client$Response<T>;
   }
 
   /**
    * Represents an HTTP Batch operation. Individual HTTP requests are added with the add method and the batch is executed using execute.
    */
-  declare type client$Batch<T> = {
+  declare type gapi$client$Batch<T> = {
     /**
      * Adds a gapi.client.Request to the batch.
      * @param request The HTTP request to add to this batch.
      * @param opt_params extra parameters for this batch entry.
      */
     add<T>(
-      client$request: client$Request<T>,
+      request: gapi$client$Request<T>,
       opt_params?: {
         /**
          * Identifies the response for this request in the map of batch responses. If one is not provided, the system generates a random ID.
          */
         id: string,
         callback(
-          individualResponse: client$Response<T>,
+          individualResponse: gapi$client$Response<T>,
           rawBatchResponse: string
         ): any
       }
@@ -124,28 +127,28 @@ declare module "gapi.client" {
      */
     execute(
       callback: (
-        responseMap: client$ResponseMap<T>,
+        responseMap: gapi$client$ResponseMap<T>,
         rawBatchResponse: string
       ) => any
     ): void
-  } & PromiseLike<client$Response<client$ResponseMap<T>>>;
+  } & PromiseLike<gapi$client$Response<gapi$client$ResponseMap<T>>>;
 
   /**
    * Creates a batch object for batching individual requests.
    */
-  declare function client$newBatch<T>(): client$Batch<T>;
+  declare function gapi$client$newBatch<T>(): gapi$client$Batch<T>;
 
-  declare var npm$namespace$auth: {
-    authorize: typeof auth$authorize,
-    init: typeof auth$init,
-    getToken: typeof auth$getToken,
-    setToken: typeof auth$setToken
+  declare var npm$namespace$gapi$auth: {
+    authorize: typeof gapi$auth$authorize,
+    init: typeof gapi$auth$init,
+    getToken: typeof gapi$auth$getToken,
+    setToken: typeof gapi$auth$setToken
   };
 
   /**
    * The OAuth 2.0 token object represents the OAuth 2.0 token and any associated data.
    */
-  declare interface auth$GoogleApiOAuth2TokenObject {
+  declare interface gapi$auth$GoogleApiOAuth2TokenObject {
     /**
      * The OAuth 2.0 token. Only present in successful responses
      */
@@ -168,13 +171,13 @@ declare module "gapi.client" {
   }
 
   /**
- * Initiates the OAuth 2.0 authorization process. The browser displays a popup window prompting the user authenticate and authorize.
- * After the user authorizes, the popup closes and the callback function fires.
- * @param params A key/value map of parameters for the request. If the key is not one of the expected OAuth 2.0 parameters, it is added to the
-URI as a query parameter.
- * @param callback The function to call once the login process is complete. The function takes an OAuth 2.0 token object as its only parameter.
- */
-  declare function auth$authorize(
+   * Initiates the OAuth 2.0 authorization process. The browser displays a popup window prompting the user authenticate and authorize.
+   * After the user authorizes, the popup closes and the callback function fires.
+   * @param params A key/value map of parameters for the request. If the key is not one of the expected OAuth 2.0 parameters, it is added to the
+   * URI as a query parameter.
+   * @param callback The function to call once the login process is complete. The function takes an OAuth 2.0 token object as its only parameter.
+   */
+  declare function gapi$auth$authorize(
     params: {
       /**
        * The application's client ID. Visit the Google Developers Console to get an OAuth 2.0 client ID.
@@ -196,24 +199,26 @@ URI as a query parameter.
        */
       scope?: string | string[]
     },
-    callback: (authResult: auth$GoogleApiOAuth2TokenObject) => void
+    callback: (authResult: gapi$auth$GoogleApiOAuth2TokenObject) => void
   ): void;
 
   /**
    * Initializes the authorization feature. Call this when the client loads to prevent popup blockers from blocking the auth window on gapi.auth.authorize calls.
    * @param callback A callback to execute when the auth feature is ready to make authorization calls.
    */
-  declare function auth$init(callback: () => any): void;
+  declare function gapi$auth$init(callback: () => any): void;
 
   /**
    * Retrieves the OAuth 2.0 token for the application.
    * @return The OAuth 2.0 token.
    */
-  declare function auth$getToken(): auth$GoogleApiOAuth2TokenObject;
+  declare function gapi$auth$getToken(): gapi$auth$GoogleApiOAuth2TokenObject;
 
   /**
    * Sets the OAuth 2.0 token for the application.
    * @param token The token to set.
    */
-  declare function auth$setToken(token: auth$GoogleApiOAuth2TokenObject): void;
+  declare function gapi$auth$setToken(
+    token: gapi$auth$GoogleApiOAuth2TokenObject
+  ): void;
 }
