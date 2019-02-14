aa59360d9f6512d6f3b25b0ac4eecec34feb2013
andretshurotshka <andwebar@gmail.com>
2019-02-14 12:38:47 +0500

    [leaflet-imageoverlay-rotated] update types

--- flow-types/types/leaflet-imageoverlay-rotated_vx.x.x/flow_v0.25.x-/leaflet-imageoverlay-rotated.js
+++ flow-types/types/leaflet-imageoverlay-rotated_vx.x.x/flow_v0.25.x-/leaflet-imageoverlay-rotated.js
@@ -1,21 +1,22 @@
-declare var npm$namespace$imageOverlay: {
-  rotated: typeof imageOverlay$rotated
-};
-declare function imageOverlay$rotated(
-  imgSrc: string | HTMLImageElement | HTMLCanvasElement,
-  topleft: LatLngExpression,
-  topright: LatLngExpression,
-  bottomleft: LatLngExpression,
-  options?: ImageOverlayOptions
-): ImageOverlay$ImageOverlay$Rotated;
-declare type ImageOverlay$Rotated = {
-  reposition(
+declare module "leaflet" {
+  declare type ImageOverlay$Rotated = {
+    reposition(
+      topleft: LatLngExpression,
+      topright: LatLngExpression,
+      bottomleft: LatLngExpression
+    ): void
+  } & ImageOverlay;
+
+  declare var npm$namespace$imageOverlay: {
+    rotated: typeof imageOverlay$rotated
+  };
+  declare function imageOverlay$rotated(
+    imgSrc: string | HTMLImageElement | HTMLCanvasElement,
     topleft: LatLngExpression,
     topright: LatLngExpression,
-    bottomleft: LatLngExpression
-  ): void
-} & ImageOverlay;
-declare module "leaflet" {
+    bottomleft: LatLngExpression,
+    options?: ImageOverlayOptions
+  ): ImageOverlay$Rotated;
 }
 declare module "leaflet-imageoverlay-rotated" {
   import typeof * as L from "leaflet";
