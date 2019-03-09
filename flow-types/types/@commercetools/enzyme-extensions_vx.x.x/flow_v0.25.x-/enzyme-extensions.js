d7c68b83ebba5a31f27306637457428157ccc754
andretshurotshka <andwebar@gmail.com>
2019-03-10 04:34:02 +0500

    [@commercetools/enzyme-extensions] update types

--- flow-types/types/@commercetools/enzyme-extensions_vx.x.x/flow_v0.25.x-/enzyme-extensions.js
+++ flow-types/types/@commercetools/enzyme-extensions_vx.x.x/flow_v0.25.x-/enzyme-extensions.js
@@ -4,14 +4,16 @@ declare module "enzyme" {
   }
   declare interface ShallowWrapper<P = {}> {
     renderProp(propName: string, ...args: any[]): ShallowWrapper<P>;
-    drill(expander: (props: any) => ShallowWrapper): ShallowWrapper<P>;
+    drill(expander: (props: any) => ShallowWrapper<>): ShallowWrapper<P>;
     until(selector: EnzymeSelector, options?: UntilOptions): ShallowWrapper<P>;
   }
 }
 declare module "@commercetools/enzyme-extensions" {
   import typeof * as enzyme from "enzyme";
 
-  declare function monkeyPatchShallowWrapper(s: typeof undefined): void;
+  declare function monkeyPatchShallowWrapper(
+    s: typeof enzyme.ShallowWrapper
+  ): void;
 
-  declare module.exports: typeof monkeyPatchShallowWrapper;
+  declare export default typeof monkeyPatchShallowWrapper;
 }
