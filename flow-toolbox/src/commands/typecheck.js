cc61d45fa3c5c844023d7a0fb493785903d58579
andretshurotshka <andwebar@gmail.com>
2019-03-10 01:17:19 +0500

    Fix typecheck script

--- flow-toolbox/src/commands/typecheck.js
+++ flow-toolbox/src/commands/typecheck.js
@@ -9,7 +9,7 @@ const { originalName } = require("../utils");
 
 const baseDir = path.join(__dirname, "../../../");
 
-async function typecheck(argv) {
+async function globalTypes() {
   const flowTypes = await glob(
     `${baseDir}/flow-types/types/**/*_v*/!(test_)*.js`
   );
@@ -28,13 +28,40 @@ async function typecheck(argv) {
         .split("_vx.x.x")[0]
     ])
   ];
+  return types
+}
+
+async function localTypes(names) {
+  const types = (await Promise.all(names.map(async name => {
+    const flowTypes = await glob(
+      `${baseDir}/flow-types/types/**/*_v*/!(test_)${name}.js`
+    );
+    const flowTypesUnformatted = await glob(
+      `${baseDir}/flow-types/unformatted/**/*_v*/!(test_)${name}.js`
+    );
+    return [flowTypes.map(p => [
+      p,
+      p.replace(path.join(baseDir, `flow-types/types/`), "").split("_vx.x.x")[0],
+    ]), flowTypesUnformatted.map(p => [
+      p,
+      p
+        .replace(path.join(baseDir, `flow-types/unformatted/`), "")
+        .split("_vx.x.x")[0]
+    ])]
+  }))).flat(2)
+  //console.log(types)
+  return types
+}
+
+async function typecheck(names) {
+  const types = names ? await localTypes(names) : await globalTypes()
   for (const [type, name] of types) {
     console.log(name, type);
     try {
       const data = await execa("flow", ["focus-check", type]);
       console.log("Ok!");
     } catch (err) {
-      console.log("Error!");
+      console.log("Error!", err);
       await fs.outputFile(
         `${baseDir}/flow-types/typecheck/${name}.log`,
         err.stdout
