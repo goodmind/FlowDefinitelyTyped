b6d3b1f1e394ef4c74b61132863626b03d35ee74
andretshurotshka <andwebar@gmail.com>
2019-03-10 04:36:22 +0500

    [@feathersjs/feathers] update types

--- flow-types/types/@feathersjs/feathers_vx.x.x/flow_v0.25.x-/feathers.js
+++ flow-types/types/@feathersjs/feathers_vx.x.x/flow_v0.25.x-/feathers.js
@@ -3,10 +3,10 @@ declare module "@feathersjs/feathers" {
 
   import typeof * as self from "@feathersjs/feathers";
 
-  declare var feathers: (<T>() => expressWs$Application<T>) & typeof self;
+  declare var feathers: (<T>() => Application<T>) & typeof self;
   declare export default typeof feathers;
 
-  declare export var main$version: string;
+  declare export var version: string;
   declare export var SKIP: SkipSymbol;
   declare export type Id = number | string;
   declare export type NullableId = Id | null;
@@ -20,21 +20,21 @@ declare module "@feathersjs/feathers" {
   declare export type ClientSideParams = Pick<Params, "query" | "paginate">;
   declare export type ServerSideParams = Params;
   declare export interface Params {
-    esri$query?: esri$Query;
-    paginate?: false | Pick<ServiceBus$PaginationOptions, "max">;
+    query?: Query;
+    paginate?: false | Pick<PaginationOptions, "max">;
     [key: string]: any;
   }
   declare export interface Paginated<T> {
     total: number;
     limit: number;
     skip: number;
-    main$data: T[];
+    data: T[];
   }
   declare export type Hook = (
-    hook: HookContext
+    hook: HookContext<>
   ) =>
-    | promise$Promise<HookContext | SkipSymbol | void>
-    | HookContext
+    | Promise<HookContext<> | SkipSymbol | void>
+    | HookContext<>
     | SkipSymbol
     | void;
   declare export type SkipSymbol = Symbol | "__feathersSkipHooks";
@@ -43,19 +43,19 @@ declare module "@feathersjs/feathers" {
      * A read only property that contains the Feathers application object. This can be used to
      * retrieve other services (via context.app.service('name')) or configuration values.
      */
-    +app: expressWs$Application;
+    +app: Application<>;
 
     /**
      * A writeable property containing the data of a create, update and patch service
      * method call.
      */
-    main$data?: T;
+    data?: T;
 
     /**
      * A writeable property with the error object that was thrown in a failed method call.
      * It is only available in error hooks.
      */
-    log$error?: any;
+    error?: any;
 
     /**
      * A writeable property and the id for a get, remove, update and patch service
@@ -80,7 +80,7 @@ declare module "@feathersjs/feathers" {
      * A read only property and contains the service name (or path) without leading or
      * trailing slashes.
      */
-    +skin$path: string;
+    +path: string;
 
     /**
      * A writeable property containing the result of the successful service method call.
@@ -96,7 +96,7 @@ declare module "@feathersjs/feathers" {
     /**
      * A read only property and contains the service this hook currently runs on.
      */
-    +service: esri$Service<T>;
+    +service: Service<T>;
 
     /**
      * A writeable, optional property and contains a "safe" version of the data that
@@ -114,13 +114,13 @@ declare module "@feathersjs/feathers" {
     /**
      * A read only property with the hook type (one of before, after or error).
      */
-    +notification$type: "before" | "after" | "error";
+    +type: "before" | "after" | "error";
   }
   declare export interface HookMap {
-    promise$all: Hook | Hook[];
-    esri$find: Hook | Hook[];
-    Reflect$get: Hook | Hook[];
-    errors$create: Hook | Hook[];
+    all: Hook | Hook[];
+    find: Hook | Hook[];
+    get: Hook | Hook[];
+    create: Hook | Hook[];
     update: Hook | Hook[];
     patch: Hook | Hook[];
     remove: Hook | Hook[];
@@ -128,37 +128,26 @@ declare module "@feathersjs/feathers" {
   declare export interface HooksObject {
     before: $Shape<HookMap>;
     after: $Shape<HookMap>;
-    log$error: $Shape<HookMap>;
+    error: $Shape<HookMap>;
   }
   declare export interface ServiceMethods<T> {
-    esri$find(params?: Params): promise$Promise<T | T[] | Paginated<T>>;
-    Reflect$get(id: Id, params?: Params): promise$Promise<T>;
-    errors$create(
-      main$data: $Shape<T> | core$Array<$Shape<T>>,
+    find(params?: Params): Promise<T | T[] | Paginated<T>>;
+    get(id: Id, params?: Params): Promise<T>;
+    create(
+      data: $Shape<T> | Array<$Shape<T>>,
       params?: Params
-    ): promise$Promise<T | T[]>;
-    update(id: NullableId, main$data: T, params?: Params): promise$Promise<T>;
-    patch(
-      id: NullableId,
-      main$data: $Shape<T>,
-      params?: Params
-    ): promise$Promise<T>;
-    remove(id: NullableId, params?: Params): promise$Promise<T>;
+    ): Promise<T | T[]>;
+    update(id: NullableId, data: T, params?: Params): Promise<T>;
+    patch(id: NullableId, data: $Shape<T>, params?: Params): Promise<T>;
+    remove(id: NullableId, params?: Params): Promise<T>;
   }
   declare export interface SetupMethod {
-    Trig$setup(app: expressWs$Application, skin$path: string): void;
+    setup(app: Application<>, path: string): void;
   }
   declare export interface ServiceOverloads<T> {
-    errors$create(
-      main$data: core$Array<$Shape<T>>,
-      params?: Params
-    ): promise$Promise<T[]>;
-    errors$create(main$data: $Shape<T>, params?: Params): promise$Promise<T>;
-    patch(
-      id: NullableId,
-      main$data: Pick<T, $Keys<T>>,
-      params?: Params
-    ): promise$Promise<T>;
+    create(data: Array<$Shape<T>>, params?: Params): Promise<T[]>;
+    create(data: $Shape<T>, params?: Params): Promise<T>;
+    patch(id: NullableId, data: Pick<T, $Keys<T>>, params?: Params): Promise<T>;
   }
   declare export type ServiceAddons<T> = {
     hooks(hooks: $Shape<HooksObject>): this
@@ -168,26 +157,24 @@ declare module "@feathersjs/feathers" {
     ServiceAddons<T> &
     ServiceMethods<T>;
   declare export type Application<ServiceTypes = any> = {
-    Reflect$get(skin$name: string): any,
-    Reflect$set(skin$name: string, value: any): this,
-    disable(skin$name: string): this,
-    disabled(skin$name: string): boolean,
-    enable(skin$name: string): this,
-    debug$enabled(skin$name: string): boolean,
-    Babelify$configure(braintree$callback: (app: this) => void): this,
+    get(name: string): any,
+    set(name: string, value: any): this,
+    disable(name: string): this,
+    disabled(name: string): boolean,
+    enable(name: string): this,
+    enabled(name: string): boolean,
+    configure(callback: (app: this) => void): this,
     hooks(hooks: $Shape<HooksObject>): this,
-    Trig$setup(server?: any): this,
+    setup(server?: any): this,
     service<L: $Keys<ServiceTypes>>(
-      __esri$location: L
-    ): esri$Service<$ElementType<ServiceTypes, L>>,
-    service(__esri$location: string): esri$Service<any>,
+      location: L
+    ): Service<$ElementType<ServiceTypes, L>>,
+    service(location: string): Service<any>,
     use(
-      skin$path: string,
-      service:
-        | $Shape<ServiceMethods<any> & SetupMethod>
-        | expressWs$Application,
-      notification$options?: any
+      path: string,
+      service: $Shape<ServiceMethods<any> & SetupMethod> | Application<>,
+      options?: any
     ): this,
-    main$version: string
+    version: string
   } & EventEmitter;
 }
