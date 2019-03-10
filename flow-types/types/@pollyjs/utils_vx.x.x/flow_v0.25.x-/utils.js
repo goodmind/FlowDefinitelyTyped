aca2ebf0862c7947a497cfcfef951a2efac15ab2
andretshurotshka <andwebar@gmail.com>
2019-03-10 08:42:32 +0500

    [@pollyjs/utils] update types

--- flow-types/types/@pollyjs/utils_vx.x.x/flow_v0.25.x-/utils.js
+++ flow-types/types/@pollyjs/utils_vx.x.x/flow_v0.25.x-/utils.js
@@ -1,16 +1,8 @@
 declare module "@pollyjs/utils" {
-  declare export class MODES {
-    constructor(...args: empty): mixed;
-    static +RECORD: Class<MODES__RECORD> & MODES__RECORD & "record"; // "record"
-    static +REPLAY: Class<MODES__REPLAY> & MODES__REPLAY & "replay"; // "replay"
-    static +PASSTHROUGH: Class<MODES__PASSTHROUGH> &
-      MODES__PASSTHROUGH &
-      "passthrough"; // "passthrough"
-    static +STOPPED: Class<MODES__STOPPED> & MODES__STOPPED & "stopped"; // "stopped"
-  }
-
-  declare class MODES__RECORD mixins MODES {}
-  declare class MODES__REPLAY mixins MODES {}
-  declare class MODES__PASSTHROUGH mixins MODES {}
-  declare class MODES__STOPPED mixins MODES {}
+  declare export var MODES: {|
+    +RECORD: "record", // "record"
+    +REPLAY: "replay", // "replay"
+    +PASSTHROUGH: "passthrough", // "passthrough"
+    +STOPPED: "stopped" // "stopped"
+  |};
 }
