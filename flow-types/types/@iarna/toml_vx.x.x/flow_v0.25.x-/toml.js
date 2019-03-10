07f49b1004fa50552ab3804396e1f03dcccfa1f9
andretshurotshka <andwebar@gmail.com>
2019-03-10 08:42:29 +0500

    [@iarna/toml] update types

--- flow-types/types/@iarna/toml_vx.x.x/flow_v0.25.x-/toml.js
+++ flow-types/types/@iarna/toml_vx.x.x/flow_v0.25.x-/toml.js
@@ -1,4 +1,10 @@
 declare module "@iarna/toml" {
+  import typeof * as parseAsync from "./parse-async";
+
+  import typeof * as parseStream from "./parse-stream";
+
+  import typeof * as stringify from "./stringify";
+
   /**
    * Synchronously parse a TOML string and return an object.
    */
