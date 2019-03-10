6bd2a29fedb83e515be0df1552f0d9d3bdaa4e5e
andretshurotshka <andwebar@gmail.com>
2019-03-10 08:43:47 +0500

    [signalr] update types

--- flow-types/types/signalr_vx.x.x/flow_v0.25.x-/signalr.js
+++ flow-types/types/signalr_vx.x.x/flow_v0.25.x-/signalr.js
@@ -1,28 +1,14 @@
 declare module "signalr" {
-  declare class SignalR$ConnectionState {
-    constructor(...args: empty): mixed;
-    static +Connecting: Class<SignalR$ConnectionState__Connecting> &
-      SignalR$ConnectionState__Connecting &
-      0; // 0
-    static +Connected: Class<SignalR$ConnectionState__Connected> &
-      SignalR$ConnectionState__Connected &
-      1; // 1
-    static +Reconnecting: Class<SignalR$ConnectionState__Reconnecting> &
-      SignalR$ConnectionState__Reconnecting &
-      2; // 2
-    static +Disconnected: Class<SignalR$ConnectionState__Disconnected> &
-      SignalR$ConnectionState__Disconnected &
-      4; // 4
-  }
+  declare var npm$namespace$SignalR: {
+    ConnectionState: typeof SignalR$ConnectionState
+  };
 
-  declare class SignalR$ConnectionState__Connecting
-    mixins SignalR$ConnectionState {}
-  declare class SignalR$ConnectionState__Connected
-    mixins SignalR$ConnectionState {}
-  declare class SignalR$ConnectionState__Reconnecting
-    mixins SignalR$ConnectionState {}
-  declare class SignalR$ConnectionState__Disconnected
-    mixins SignalR$ConnectionState {}
+  declare var SignalR$ConnectionState: {|
+    +Connecting: 0, // 0
+    +Connected: 1, // 1
+    +Reconnecting: 2, // 2
+    +Disconnected: 4 // 4
+  |};
 
   declare interface SignalR$AvailableEvents {
     onStart: string;
@@ -38,16 +24,16 @@ declare module "signalr" {
   declare interface SignalR$Transport {
     name: string;
     supportsKeepAlive(): boolean;
-    send(connection: SignalR$SignalR$Connection, data: any): void;
+    send(connection: SignalR$Connection, data: any): void;
     start(
-      connection: SignalR$SignalR$Connection,
+      connection: SignalR$Connection,
       onSuccess: () => void,
       onFailed: (error?: SignalR$ConnectionError) => void
     ): void;
-    reconnect(connection: SignalR$SignalR$Connection): void;
-    lostConnection(connection: SignalR$SignalR$Connection): void;
-    stop(connection: SignalR$SignalR$Connection): void;
-    abort(connection: SignalR$SignalR$Connection, async: boolean): void;
+    reconnect(connection: SignalR$Connection): void;
+    lostConnection(connection: SignalR$Connection): void;
+    stop(connection: SignalR$Connection): void;
+    abort(connection: SignalR$Connection, async: boolean): void;
   }
 
   declare interface SignalR$Transports {
@@ -57,7 +43,7 @@ declare module "signalr" {
     webSockets: SignalR$Transport;
   }
 
-  declare interface Hub$Proxy {
+  declare interface SignalR$Hub$Proxy {
     state: any;
     connection: SignalR$Connection;
     hubName: string;
@@ -69,14 +55,17 @@ declare module "signalr" {
      * @param eventName The name of the hub event to register the callback for.
      * @param callback The callback to be invoked.
      */
-    on(eventName: string, callback: (...msg: any[]) => void): Hub$Proxy;
+    on(eventName: string, callback: (...msg: any[]) => void): SignalR$Hub$Proxy;
 
     /**
      * Removes the callback invocation request from the server hub for the given event name.
      * @param eventName The name of the hub event to unregister the callback for.
      * @param callback The callback to be invoked.
      */
-    off(eventName: string, callback: (...msg: any[]) => void): Hub$Proxy;
+    off(
+      eventName: string,
+      callback: (...msg: any[]) => void
+    ): SignalR$Hub$Proxy;
 
     /**
      * Invokes a server hub method with the given arguments.
@@ -85,20 +74,20 @@ declare module "signalr" {
     invoke(methodName: string, ...args: any[]): JQueryPromise<any>;
   }
 
-  declare interface Hub$Options {
+  declare interface SignalR$Hub$Options {
     qs?: string;
     logging?: boolean;
     useDefaultPath?: boolean;
   }
 
-  declare interface Hub$ClientHubInvocation {
+  declare interface SignalR$Hub$ClientHubInvocation {
     Hub: string;
     Method: string;
     Args: string;
     State: string;
   }
 
-  declare type Hub$Connection = {
+  declare type SignalR$Hub$Connection = {
     proxies: {
       [hubName: string]: any
     },
@@ -112,150 +101,19 @@ declare module "signalr" {
      * methods on server hubs and handle client method invocation requests from the server.
      * @param hubName The name of the hub on the server to create the proxy for.
      */
-    createHubProxy(hubName: string): Hub$Proxy,
-    clientProtocol: string,
-    ajaxDataType: string,
-    contentType: string,
-    id: string,
-    json: JSON,
-    logging: boolean,
-    url: string,
-    qs: string | Object,
-    state: number,
-    reconnectDelay: number,
-    transportConnectTimeout: number,
+    createHubProxy(hubName: string): SignalR$Hub$Proxy
+  } & SignalR$Connection;
 
-    /**
-     * This should be set by the server in response to the negotiate request (30s default)
-     */
-    disconnectTimeout: number,
-
-    /**
-     * This should be set by the server in response to the negotiate request
-     */
-    reconnectWindow: number,
-
-    /**
-     * Warn user of slow connection if we breach the X% mark of the keep alive timeout
-     */
-    keepAliveWarnAt: number,
-
-    /**
-     * Starts the connection
-     */
-    start(): JQueryPromise<any>,
-
-    /**
-     * Starts the connection
-     * @param callback A callback function to execute when the connection has started
-     */
-    start(callback: () => void): JQueryPromise<any>,
-
-    /**
-     * Starts the connection
-     * @param options Options map
-     */
-    start(options: SignalR$ConnectionOptions): JQueryPromise<any>,
-
-    /**
-     * Starts the connection
-     * @param options Options map
-     * @param calback A callback function to execute when the connection has started
-     */
-    start(
-      options: SignalR$ConnectionOptions,
-      callback: () => void
-    ): JQueryPromise<any>,
-
-    /**
-     * Adds a callback that will be invoked before anything is sent over the connection
-     * @param calback A callback function to execute before the connection is fully instantiated.
-     */
-    starting(callback: () => void): Hub$Connection,
-
-    /**
-     * Sends data over the connection
-     * @param options Options map
-     * @param calback The data to send over the connection
-     */
-    send(data: string): Hub$Connection,
-
-    /**
-     * Adds a callback that will be invoked after anything is received over the connection
-     * @param calback A callback function to execute when any data is received on the connection
-     */
-    received(callback: (data: any) => void): Hub$Connection,
-
-    /**
-     * Adds a callback that will be invoked when the connection state changes
-     * @param calback A callback function to execute when the connection state changes
-     */
-    stateChanged(
-      callback: (change: SignalR$StateChanged) => void
-    ): Hub$Connection,
-
-    /**
-     * Adds a callback that will be invoked after an error occurs with the connection
-     * @param calback A callback function to execute when an error occurs on the connection
-     */
-    error(callback: (error: SignalR$ConnectionError) => void): Hub$Connection,
-
-    /**
-     * Adds a callback that will be invoked when the client disconnects
-     * @param calback A callback function to execute when the connection is broken
-     */
-    disconnected(callback: () => void): Hub$Connection,
-
-    /**
-     * Adds a callback that will be invoked when the client detects a slow connection
-     * @param calback A callback function to execute when the connection is slow
-     */
-    connectionSlow(callback: () => void): Hub$Connection,
-
-    /**
-     * Adds a callback that will be invoked when the underlying transport begins reconnecting
-     * @param calback A callback function to execute when the connection enters a reconnecting state
-     */
-    reconnecting(callback: () => void): Hub$Connection,
-
-    /**
-     * Adds a callback that will be invoked when the underlying transport reconnects
-     * @param calback A callback function to execute when the connection is restored
-     */
-    reconnected(callback: () => void): Hub$Connection,
-
-    /**
-     * Stops listening
-     * @param async Whether or not to asynchronously abort the connection
-     * @param notifyServer Whether we want to notify the server that we are aborting the connection
-     */
-    stop(async?: boolean, notifyServer?: boolean): Hub$Connection,
-    log(msg: string): Hub$Connection,
-
-    /**
-     * Checks if url is cross domain
-     * @param url The base URL
-     * @param against An optional argument to compare the URL against, if not specified it will be set to window.location. If specified it must contain a protocol and a host property.
-     */
-    isCrossDomain(
-      url: string,
-      against?: Location | SignalR$SimplifyLocation
-    ): boolean,
-    hub: Hub$Hub$Connection,
-    lastError: SignalR$ConnectionError,
-    resources: SignalR$Resources
-  } & SignalR$Hub$Connection;
-
-  declare interface Hub$HubCreator {
+  declare interface SignalR$Hub$HubCreator {
     /**
      * Creates a new hub connection.
      * @param url [Optional] The hub route url, defaults to "/signalr".
      * @param options [Optional] Settings to use when creating the hubConnection.
      */
-    (url?: string, options?: Hub$Options): Hub$Connection;
+    (url?: string, options?: SignalR$Hub$Options): SignalR$Hub$Connection;
   }
 
-  declare interface Hub$IHub {
+  declare interface SignalR$Hub$IHub {
     start(): void;
   }
 
@@ -334,64 +192,50 @@ declare module "signalr" {
     source?: string
   } & Error;
 
-  declare type SignalR$Connection = {
-    proxies: {
-      [hubName: string]: any
-    },
-    transport: {
-      name: string,
-      supportsKeepAlive: () => boolean
-    },
-
-    /**
-     * Creates a new proxy object for the given hub connection that can be used to invoke
-     * methods on server hubs and handle client method invocation requests from the server.
-     * @param hubName The name of the hub on the server to create the proxy for.
-     */
-    createHubProxy(hubName: string): Hub$Proxy,
-    clientProtocol: string,
-    ajaxDataType: string,
-    contentType: string,
-    id: string,
-    json: JSON,
-    logging: boolean,
-    url: string,
-    qs: string | Object,
-    state: number,
-    reconnectDelay: number,
-    transportConnectTimeout: number,
+  declare interface SignalR$Connection {
+    clientProtocol: string;
+    ajaxDataType: string;
+    contentType: string;
+    id: string;
+    json: JSON;
+    logging: boolean;
+    url: string;
+    qs: string | Object;
+    state: number;
+    reconnectDelay: number;
+    transportConnectTimeout: number;
 
     /**
      * This should be set by the server in response to the negotiate request (30s default)
      */
-    disconnectTimeout: number,
+    disconnectTimeout: number;
 
     /**
      * This should be set by the server in response to the negotiate request
      */
-    reconnectWindow: number,
+    reconnectWindow: number;
 
     /**
      * Warn user of slow connection if we breach the X% mark of the keep alive timeout
      */
-    keepAliveWarnAt: number,
+    keepAliveWarnAt: number;
 
     /**
      * Starts the connection
      */
-    start(): JQueryPromise<any>,
+    start(): JQueryPromise<any>;
 
     /**
      * Starts the connection
      * @param callback A callback function to execute when the connection has started
      */
-    start(callback: () => void): JQueryPromise<any>,
+    start(callback: () => void): JQueryPromise<any>;
 
     /**
      * Starts the connection
      * @param options Options map
      */
-    start(options: SignalR$ConnectionOptions): JQueryPromise<any>,
+    start(options: SignalR$ConnectionOptions): JQueryPromise<any>;
 
     /**
      * Starts the connection
@@ -401,26 +245,26 @@ declare module "signalr" {
     start(
       options: SignalR$ConnectionOptions,
       callback: () => void
-    ): JQueryPromise<any>,
+    ): JQueryPromise<any>;
 
     /**
      * Adds a callback that will be invoked before anything is sent over the connection
      * @param calback A callback function to execute before the connection is fully instantiated.
      */
-    starting(callback: () => void): SignalR$Connection,
+    starting(callback: () => void): SignalR$Connection;
 
     /**
      * Sends data over the connection
      * @param options Options map
      * @param calback The data to send over the connection
      */
-    send(data: string): SignalR$Connection,
+    send(data: string): SignalR$Connection;
 
     /**
      * Adds a callback that will be invoked after anything is received over the connection
      * @param calback A callback function to execute when any data is received on the connection
      */
-    received(callback: (data: any) => void): SignalR$Connection,
+    received(callback: (data: any) => void): SignalR$Connection;
 
     /**
      * Adds a callback that will be invoked when the connection state changes
@@ -428,7 +272,7 @@ declare module "signalr" {
      */
     stateChanged(
       callback: (change: SignalR$StateChanged) => void
-    ): SignalR$Connection,
+    ): SignalR$Connection;
 
     /**
      * Adds a callback that will be invoked after an error occurs with the connection
@@ -436,39 +280,39 @@ declare module "signalr" {
      */
     error(
       callback: (error: SignalR$ConnectionError) => void
-    ): SignalR$Connection,
+    ): SignalR$Connection;
 
     /**
      * Adds a callback that will be invoked when the client disconnects
      * @param calback A callback function to execute when the connection is broken
      */
-    disconnected(callback: () => void): SignalR$Connection,
+    disconnected(callback: () => void): SignalR$Connection;
 
     /**
      * Adds a callback that will be invoked when the client detects a slow connection
      * @param calback A callback function to execute when the connection is slow
      */
-    connectionSlow(callback: () => void): SignalR$Connection,
+    connectionSlow(callback: () => void): SignalR$Connection;
 
     /**
      * Adds a callback that will be invoked when the underlying transport begins reconnecting
      * @param calback A callback function to execute when the connection enters a reconnecting state
      */
-    reconnecting(callback: () => void): SignalR$Connection,
+    reconnecting(callback: () => void): SignalR$Connection;
 
     /**
      * Adds a callback that will be invoked when the underlying transport reconnects
      * @param calback A callback function to execute when the connection is restored
      */
-    reconnected(callback: () => void): SignalR$Connection,
+    reconnected(callback: () => void): SignalR$Connection;
 
     /**
      * Stops listening
      * @param async Whether or not to asynchronously abort the connection
      * @param notifyServer Whether we want to notify the server that we are aborting the connection
      */
-    stop(async?: boolean, notifyServer?: boolean): SignalR$Connection,
-    log(msg: string): SignalR$Connection,
+    stop(async?: boolean, notifyServer?: boolean): SignalR$Connection;
+    log(msg: string): SignalR$Connection;
 
     /**
      * Checks if url is cross domain
@@ -478,12 +322,11 @@ declare module "signalr" {
     isCrossDomain(
       url: string,
       against?: Location | SignalR$SimplifyLocation
-    ): boolean,
-    hub: Hub$SignalR$Connection,
-    lastError: SignalR$ConnectionError,
-    resources: SignalR$Resources
-  } & SignalR$SignalR$Connection;
-
+    ): boolean;
+    hub: SignalR$Hub$Connection;
+    lastError: SignalR$ConnectionError;
+    resources: SignalR$Resources;
+  }
   declare interface SignalR {
     /**
      * Creates a new SignalR connection for the given url
@@ -495,24 +338,24 @@ declare module "signalr" {
       url: string,
       queryString?: string | Object,
       logging?: boolean
-    ): SignalR$SignalR$Connection;
-    ajaxDefaults: SignalR$SignalR$AjaxDefaults;
+    ): SignalR$Connection;
+    ajaxDefaults: SignalR$AjaxDefaults;
     changeState(
-      connection: SignalR$SignalR$Connection,
+      connection: SignalR$Connection,
       expectedState: number,
       newState: number
     ): void;
-    connectionState: SignalR$SignalR$ConnectionStates;
-    events: SignalR$SignalR$AvailableEvents;
-    transports: SignalR$SignalR$Transports;
-    hub: SignalR$Hub.SignalR$Connection;
-    hubConnection: SignalR$Hub.Hub$HubCreator;
-    isDisconnecting(connection: SignalR$SignalR$Connection): boolean;
+    connectionState: SignalR$ConnectionStates;
+    events: SignalR$AvailableEvents;
+    transports: SignalR$Transports;
+    hub: SignalR$Hub$Connection;
+    hubConnection: SignalR$Hub$HubCreator;
+    isDisconnecting(connection: SignalR$Connection): boolean;
 
     /**
      * Reinstates the original value of $.connection and returns the signalR object for manual assignment.
      */
-    noConflict(): SignalR$SignalR$Connection;
+    noConflict(): SignalR$Connection;
 
     /**
      * Current SignalR version.
@@ -522,6 +365,6 @@ declare module "signalr" {
   declare interface JQueryStatic {
     signalR: SignalR;
     connection: SignalR;
-    hubConnection: SignalR$Hub.Hub$HubCreator;
+    hubConnection: SignalR$Hub$HubCreator;
   }
 }
