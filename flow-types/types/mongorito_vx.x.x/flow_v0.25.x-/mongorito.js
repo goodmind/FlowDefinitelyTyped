b06b0401a22c253fab09c4a486bf11316cc03cb9
andretshurotshka <andwebar@gmail.com>
2019-03-10 08:43:37 +0500

    [mongorito] update types

--- flow-types/types/mongorito_vx.x.x/flow_v0.25.x-/mongorito.js
+++ flow-types/types/mongorito_vx.x.x/flow_v0.25.x-/mongorito.js
@@ -101,142 +101,91 @@ declare module "mongorito" {
     refresh(): Promise<RefreshedAction>;
     remove(): Promise<RemovedAction>;
   }
-  declare export class ActionTypes {
-    constructor(...args: empty): mixed;
-    static +GET: Class<ActionTypes__GET> & ActionTypes__GET & "@@mongorito/GET"; // "@@mongorito/GET"
-    static +SET: Class<ActionTypes__SET> & ActionTypes__SET & "@@mongorito/SET"; // "@@mongorito/SET"
-    static +UNSET: Class<ActionTypes__UNSET> &
-      ActionTypes__UNSET &
-      "@@mongorito/UNSET"; // "@@mongorito/UNSET"
-    static +REFRESH: Class<ActionTypes__REFRESH> &
-      ActionTypes__REFRESH &
-      "@@mongorito/REFRESH"; // "@@mongorito/REFRESH"
-    static +REFRESHED: Class<ActionTypes__REFRESHED> &
-      ActionTypes__REFRESHED &
-      "@@mongorito/REFRESHED"; // "@@mongorito/REFRESHED"
-    static +SAVE: Class<ActionTypes__SAVE> &
-      ActionTypes__SAVE &
-      "@@mongorito/SAVE"; // "@@mongorito/SAVE"
-    static +CREATE: Class<ActionTypes__CREATE> &
-      ActionTypes__CREATE &
-      "@@mongorito/CREATE"; // "@@mongorito/CREATE"
-    static +CREATED: Class<ActionTypes__CREATED> &
-      ActionTypes__CREATED &
-      "@@mongorito/CREATED"; // "@@mongorito/CREATED"
-    static +UPDATE: Class<ActionTypes__UPDATE> &
-      ActionTypes__UPDATE &
-      "@@mongorito/UPDATE"; // "@@mongorito/UPDATE"
-    static +UPDATED: Class<ActionTypes__UPDATED> &
-      ActionTypes__UPDATED &
-      "@@mongorito/UPDATED"; // "@@mongorito/UPDATED"
-    static +REMOVE: Class<ActionTypes__REMOVE> &
-      ActionTypes__REMOVE &
-      "@@mongorito/REMOVE"; // "@@mongorito/REMOVE"
-    static +REMOVED: Class<ActionTypes__REMOVED> &
-      ActionTypes__REMOVED &
-      "@@mongorito/REMOVED"; // "@@mongorito/REMOVED"
-    static +INCREMENT: Class<ActionTypes__INCREMENT> &
-      ActionTypes__INCREMENT &
-      "@@mongorito/INCREMENT"; // "@@mongorito/INCREMENT"
-    static +CREATE_INDEX: Class<ActionTypes__CREATE_INDEX> &
-      ActionTypes__CREATE_INDEX &
-      "@@mongorito/CREATE_INDEX"; // "@@mongorito/CREATE_INDEX"
-    static +DROP_INDEX: Class<ActionTypes__DROP_INDEX> &
-      ActionTypes__DROP_INDEX &
-      "@@mongorito/DROP_INDEX"; // "@@mongorito/DROP_INDEX"
-    static +LIST_INDEXES: Class<ActionTypes__LIST_INDEXES> &
-      ActionTypes__LIST_INDEXES &
-      "@@mongorito/LIST_INDEXES"; // "@@mongorito/LIST_INDEXES"
-    static +QUERY: Class<ActionTypes__QUERY> &
-      ActionTypes__QUERY &
-      "@@mongorito/QUERY"; // "@@mongorito/QUERY"
-    static +CALL: Class<ActionTypes__CALL> &
-      ActionTypes__CALL &
-      "@@mongorito/CALL"; // "@@mongorito/CALL"
-  }
 
-  declare class ActionTypes__GET mixins ActionTypes {}
-  declare class ActionTypes__SET mixins ActionTypes {}
-  declare class ActionTypes__UNSET mixins ActionTypes {}
-  declare class ActionTypes__REFRESH mixins ActionTypes {}
-  declare class ActionTypes__REFRESHED mixins ActionTypes {}
-  declare class ActionTypes__SAVE mixins ActionTypes {}
-  declare class ActionTypes__CREATE mixins ActionTypes {}
-  declare class ActionTypes__CREATED mixins ActionTypes {}
-  declare class ActionTypes__UPDATE mixins ActionTypes {}
-  declare class ActionTypes__UPDATED mixins ActionTypes {}
-  declare class ActionTypes__REMOVE mixins ActionTypes {}
-  declare class ActionTypes__REMOVED mixins ActionTypes {}
-  declare class ActionTypes__INCREMENT mixins ActionTypes {}
-  declare class ActionTypes__CREATE_INDEX mixins ActionTypes {}
-  declare class ActionTypes__DROP_INDEX mixins ActionTypes {}
-  declare class ActionTypes__LIST_INDEXES mixins ActionTypes {}
-  declare class ActionTypes__QUERY mixins ActionTypes {}
-  declare class ActionTypes__CALL mixins ActionTypes {}
+  declare export var ActionTypes: {|
+    +GET: "@@mongorito/GET", // "@@mongorito/GET"
+    +SET: "@@mongorito/SET", // "@@mongorito/SET"
+    +UNSET: "@@mongorito/UNSET", // "@@mongorito/UNSET"
+    +REFRESH: "@@mongorito/REFRESH", // "@@mongorito/REFRESH"
+    +REFRESHED: "@@mongorito/REFRESHED", // "@@mongorito/REFRESHED"
+    +SAVE: "@@mongorito/SAVE", // "@@mongorito/SAVE"
+    +CREATE: "@@mongorito/CREATE", // "@@mongorito/CREATE"
+    +CREATED: "@@mongorito/CREATED", // "@@mongorito/CREATED"
+    +UPDATE: "@@mongorito/UPDATE", // "@@mongorito/UPDATE"
+    +UPDATED: "@@mongorito/UPDATED", // "@@mongorito/UPDATED"
+    +REMOVE: "@@mongorito/REMOVE", // "@@mongorito/REMOVE"
+    +REMOVED: "@@mongorito/REMOVED", // "@@mongorito/REMOVED"
+    +INCREMENT: "@@mongorito/INCREMENT", // "@@mongorito/INCREMENT"
+    +CREATE_INDEX: "@@mongorito/CREATE_INDEX", // "@@mongorito/CREATE_INDEX"
+    +DROP_INDEX: "@@mongorito/DROP_INDEX", // "@@mongorito/DROP_INDEX"
+    +LIST_INDEXES: "@@mongorito/LIST_INDEXES", // "@@mongorito/LIST_INDEXES"
+    +QUERY: "@@mongorito/QUERY", // "@@mongorito/QUERY"
+    +CALL: "@@mongorito/CALL" // "@@mongorito/CALL"
+  |};
 
   declare export interface GetAction {
-    type: ActionTypes.GET;
+    type: typeof ActionTypes.GET;
     key?: string;
   }
   declare export interface SetAction {
-    type: ActionTypes.SET;
+    type: typeof ActionTypes.SET;
     fields: { [key: string]: any };
   }
   declare export interface UnsetAction {
-    type: ActionTypes.UNSET;
+    type: typeof ActionTypes.UNSET;
     keys: string | string[];
   }
   declare export interface RefreshAction {
-    type: ActionTypes.REFRESH;
+    type: typeof ActionTypes.REFRESH;
   }
   declare export interface RefreshedAction {
-    type: ActionTypes.REFRESHED;
+    type: typeof ActionTypes.REFRESHED;
     fields: { [key: string]: any };
   }
   declare export interface SaveAction {
-    type: ActionTypes.SAVE;
+    type: typeof ActionTypes.SAVE;
     fields: { [key: string]: any };
   }
   declare export interface CreateAction {
-    type: ActionTypes.CREATE;
+    type: typeof ActionTypes.CREATE;
     id: { [key: string]: any };
   }
   declare export interface CreatedAction {
-    type: ActionTypes.CREATED;
+    type: typeof ActionTypes.CREATED;
     id: { [key: string]: any };
   }
   declare export interface UpdateAction {
-    type: ActionTypes.UPDATE;
+    type: typeof ActionTypes.UPDATE;
     fields: { [key: string]: any };
   }
   declare export interface UpdatedAction {
-    type: ActionTypes.UPDATED;
+    type: typeof ActionTypes.UPDATED;
     fields: { [key: string]: any };
   }
   declare export interface RemoveAction {
-    type: ActionTypes.REMOVE;
+    type: typeof ActionTypes.REMOVE;
   }
   declare export interface RemovedAction {
-    type: ActionTypes.REMOVED;
+    type: typeof ActionTypes.REMOVED;
   }
   declare export interface IncrementAction {
-    type: ActionTypes.INCREMENT;
+    type: typeof ActionTypes.INCREMENT;
     fields: { [key: string]: any };
   }
   declare export interface CreateIndexAction {
-    type: ActionTypes.CREATE_INDEX;
+    type: typeof ActionTypes.CREATE_INDEX;
     args: any[];
   }
   declare export interface DropIndexAction {
-    type: ActionTypes.DROP_INDEX;
+    type: typeof ActionTypes.DROP_INDEX;
     args: any[];
   }
   declare export interface ListIndexesAction {
-    type: ActionTypes.LIST_INDEXES;
+    type: typeof ActionTypes.LIST_INDEXES;
     args: any[];
   }
   declare export interface QueryAction {
-    type: ActionTypes.QUERY;
+    type: typeof ActionTypes.QUERY;
     method: string;
     query: Array<{
       method: string,
@@ -244,7 +193,7 @@ declare module "mongorito" {
     }>;
   }
   declare export interface CallAction {
-    type: ActionTypes.CALL;
+    type: typeof ActionTypes.CALL;
     method: string;
     args: Array<{
       method: string,
@@ -282,38 +231,31 @@ declare module "mongorito" {
     modelClass: ModelClass;
     model?: Model;
   }
-  declare export type Reducer<S = any> = (state: S, action: Action) => Reducer;
+  declare export type Reducer<S = any> = (
+    state: S,
+    action: Action
+  ) => Reducer<>;
   declare export interface DefaultReducer {
     unset: Reducer<string[]>;
     fields: Reducer<{ [key: string]: any }>;
   }
-  declare export type ReducerState = DefaultReducer & Map<any, Reducer>;
+  declare export type ReducerState = DefaultReducer & Map<any, Reducer<>>;
   declare export type ReducerModifier = (
     reducerState: ReducerState
   ) => ReducerState;
   declare export type Plugin = (
     modelClass: ModelClass
   ) => (store: PluginStore) => (next: PluginNext) => (action: Action) => void;
-  declare export class DatabaseState {
-    constructor(...args: empty): mixed;
-    static +STATE_CONNECTED: Class<DatabaseState__STATE_CONNECTED> &
-      DatabaseState__STATE_CONNECTED &
-      0; // 0
-    static +STATE_CONNECTING: Class<DatabaseState__STATE_CONNECTING> &
-      DatabaseState__STATE_CONNECTING &
-      1; // 1
-    static +STATE_DISCONNECTED: Class<DatabaseState__STATE_DISCONNECTED> &
-      DatabaseState__STATE_DISCONNECTED &
-      2; // 2
-  }
 
-  declare class DatabaseState__STATE_CONNECTED mixins DatabaseState {}
-  declare class DatabaseState__STATE_CONNECTING mixins DatabaseState {}
-  declare class DatabaseState__STATE_DISCONNECTED mixins DatabaseState {}
+  declare export var DatabaseState: {|
+    +STATE_CONNECTED: 0, // 0
+    +STATE_CONNECTING: 1, // 1
+    +STATE_DISCONNECTED: 2 // 2
+  |};
 
   declare export class Database {
     options: MongoClientOptions;
-    state: DatabaseState;
+    state: $Values<typeof DatabaseState>;
     models: ModelClass[];
     plugins: Plugin[];
     constructor(urls?: string | string[], options?: MongoClientOptions): this;
