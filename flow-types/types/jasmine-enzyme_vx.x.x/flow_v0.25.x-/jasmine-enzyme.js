ffbe35e21135e9607addc8a69e394712f87b84f1
andretshurotshka <andwebar@gmail.com>
2019-03-11 01:13:42 +0500

    [jasmine-enzyme] update types

--- flow-types/types/jasmine-enzyme_vx.x.x/flow_v0.25.x-/jasmine-enzyme.js
+++ flow-types/types/jasmine-enzyme_vx.x.x/flow_v0.25.x-/jasmine-enzyme.js
@@ -1,23 +1,23 @@
-declare interface jasmine$Matchers<T> {
-  toBeChecked(): boolean;
-  toBeDisabled(): boolean;
-  toBeEmpty(): boolean;
-  toBePresent(): boolean;
-  toContainReact(reactInstance: JSX.Element): boolean;
-  toHaveClassName(className: string): boolean;
-  toHaveHTML(html: string): boolean;
-  toHaveProp(propKey: string, propValue?: any): boolean;
-  toHaveRef(refName: string): string;
-  toHaveState(stateKey: string, stateValue?: any): boolean;
-  toHaveStyle(styleKey: string, styleValue?: any): boolean;
-  toHaveTagName(tagName: string): boolean;
-  toHaveText(text?: string): boolean;
-  toIncludeText(text: string): boolean;
-  toHaveValue(value: any): boolean;
-  toMatchElement(reactInstance: JSX.Element): boolean;
-  toMatchSelector(selector: string): boolean;
-}
 declare module "global" {
+  declare interface jasmine$Matchers<T> {
+    toBeChecked(): boolean;
+    toBeDisabled(): boolean;
+    toBeEmpty(): boolean;
+    toBePresent(): boolean;
+    toContainReact(reactInstance: React$Node): boolean;
+    toHaveClassName(className: string): boolean;
+    toHaveHTML(html: string): boolean;
+    toHaveProp(propKey: string, propValue?: any): boolean;
+    toHaveRef(refName: string): string;
+    toHaveState(stateKey: string, stateValue?: any): boolean;
+    toHaveStyle(styleKey: string, styleValue?: any): boolean;
+    toHaveTagName(tagName: string): boolean;
+    toHaveText(text?: string): boolean;
+    toIncludeText(text: string): boolean;
+    toHaveValue(value: any): boolean;
+    toMatchElement(reactInstance: React$Node): boolean;
+    toMatchSelector(selector: string): boolean;
+  }
 }
 declare module "jasmine-enzyme" {
   declare function jasmineEnzyme(): void;
