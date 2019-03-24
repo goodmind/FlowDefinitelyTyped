f86e80ab06587bbbbb9daeb752eb694a6576e14b
andretshurotshka <andwebar@gmail.com>
2019-03-24 06:56:05 +0500

    [@carbon/type] update types

---
+++ flow-types/types/@carbon/type_vx.x.x/flow_v0.25.x-/type.js
@@ -0,0 +1,702 @@
+declare module "@carbon/type" {
+  declare export var bodyLong01: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: string,
+    lineHeight: string
+  };
+  declare export var bodyLong02: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var bodyShort01: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: string,
+    lineHeight: string
+  };
+  declare export var bodyShort02: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var caption01: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: string,
+    lineHeight: string
+  };
+  declare export var code01: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: string,
+    lineHeight: string
+  };
+  declare export var code02: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: string,
+    lineHeight: string
+  };
+  declare export var display01: {
+    "@media (min-width: 42rem)": {
+      fontSize: string
+    },
+    "@media (min-width: 66rem)": {
+      fontSize: string
+    },
+    "@media (min-width: 82rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 99rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var display02: {
+    "@media (min-width: 42rem)": {
+      fontSize: string
+    },
+    "@media (min-width: 66rem)": {
+      fontSize: string
+    },
+    "@media (min-width: 82rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 99rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var display03: {
+    "@media (min-width: 42rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 66rem)": {
+      fontSize: string,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    "@media (min-width: 82rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 99rem)": {
+      fontSize: string,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var display04: {
+    "@media (min-width: 42rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 66rem)": {
+      fontSize: string,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    "@media (min-width: 82rem)": {
+      fontSize: string,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    "@media (min-width: 99rem)": {
+      fontSize: string,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var expressiveHeading04: {
+    "@media (min-width: 82rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 99rem)": {
+      fontSize: string
+    },
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var expressiveHeading05: {
+    "@media (min-width: 42rem)": {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    "@media (min-width: 66rem)": {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    "@media (min-width: 82rem)": {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    "@media (min-width: 99rem)": {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var expressiveParagraph01: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lg: {
+      fontSize: string,
+      lineHeight: string
+    },
+    lineHeight: string,
+    max: {
+      fontSize: string,
+      lineHeight: string
+    }
+  };
+  declare export var fontFamilies: {
+    mono: string,
+    sans: string,
+    sansCondensed: string,
+    sansHebrew: string,
+    serif: string
+  };
+  declare export var fontWeights: {
+    light: number,
+    regular: number,
+    semibold: number
+  };
+  declare export var heading01: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: string,
+    lineHeight: string
+  };
+  declare export var heading02: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var heading03: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var helperText01: {
+    fontFamily: string,
+    fontSize: string,
+    fontStyle: string,
+    letterSpacing: string,
+    lineHeight: string
+  };
+  declare export var label01: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: string,
+    lineHeight: string
+  };
+  declare export var productiveHeading04: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var productiveHeading05: {
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var quotation01: {
+    "@media (min-width: 42rem)": {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number
+    },
+    "@media (min-width: 66rem)": {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    "@media (min-width: 82rem)": {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    "@media (min-width: 99rem)": {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var quotation02: {
+    "@media (min-width: 42rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 66rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 82rem)": {
+      fontSize: string,
+      lineHeight: string
+    },
+    "@media (min-width: 99rem)": {
+      fontSize: string
+    },
+    fontFamily: string,
+    fontSize: string,
+    fontWeight: number,
+    letterSpacing: number,
+    lineHeight: string
+  };
+  declare export var reset: {
+    body: {
+      "-moz-osx-font-smoothing": string,
+      "-webkit-font-smoothing": string,
+      fontFamily: string,
+      fontWeight: number,
+      textRendering: string
+    },
+    html: {
+      fontSize: string
+    },
+    strong: {
+      fontWeight: number
+    }
+  };
+  declare export var scale: number[];
+  declare export var spacing: {
+    layout01: string,
+    layout02: string,
+    layout03: string,
+    margin01: string,
+    margin02: string,
+    margin03: string
+  };
+  declare export var styles: {
+    bodyLong01: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    bodyLong02: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    bodyShort01: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    bodyShort02: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    caption01: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    code01: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    code02: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    display01: {
+      "@media (min-width: 42rem)": {
+        fontSize: string
+      },
+      "@media (min-width: 66rem)": {
+        fontSize: string
+      },
+      "@media (min-width: 82rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 99rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    display02: {
+      "@media (min-width: 42rem)": {
+        fontSize: string
+      },
+      "@media (min-width: 66rem)": {
+        fontSize: string
+      },
+      "@media (min-width: 82rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 99rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    display03: {
+      "@media (min-width: 42rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 66rem)": {
+        fontSize: string,
+        letterSpacing: string,
+        lineHeight: string
+      },
+      "@media (min-width: 82rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 99rem)": {
+        fontSize: string,
+        letterSpacing: string,
+        lineHeight: string
+      },
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    display04: {
+      "@media (min-width: 42rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 66rem)": {
+        fontSize: string,
+        letterSpacing: string,
+        lineHeight: string
+      },
+      "@media (min-width: 82rem)": {
+        fontSize: string,
+        letterSpacing: string,
+        lineHeight: string
+      },
+      "@media (min-width: 99rem)": {
+        fontSize: string,
+        letterSpacing: string,
+        lineHeight: string
+      },
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    expressiveHeading04: {
+      "@media (min-width: 82rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 99rem)": {
+        fontSize: string
+      },
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    expressiveHeading05: {
+      "@media (min-width: 42rem)": {
+        fontFamily: string,
+        fontSize: string,
+        fontWeight: number,
+        letterSpacing: number,
+        lineHeight: string
+      },
+      "@media (min-width: 66rem)": {
+        fontFamily: string,
+        fontSize: string,
+        fontWeight: number,
+        letterSpacing: number,
+        lineHeight: string
+      },
+      "@media (min-width: 82rem)": {
+        fontFamily: string,
+        fontSize: string,
+        fontWeight: number,
+        letterSpacing: number,
+        lineHeight: string
+      },
+      "@media (min-width: 99rem)": {
+        fontFamily: string,
+        fontSize: string,
+        fontWeight: number,
+        letterSpacing: number,
+        lineHeight: string
+      },
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    expressiveParagraph01: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lg: {
+        fontSize: string,
+        lineHeight: string
+      },
+      lineHeight: string,
+      max: {
+        fontSize: string,
+        lineHeight: string
+      }
+    },
+    heading01: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    heading02: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    heading03: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    helperText01: {
+      fontFamily: string,
+      fontSize: string,
+      fontStyle: string,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    label01: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: string,
+      lineHeight: string
+    },
+    productiveHeading04: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    productiveHeading05: {
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    quotation01: {
+      "@media (min-width: 42rem)": {
+        fontFamily: string,
+        fontSize: string,
+        fontWeight: number,
+        letterSpacing: number
+      },
+      "@media (min-width: 66rem)": {
+        fontFamily: string,
+        fontSize: string,
+        fontWeight: number,
+        letterSpacing: number,
+        lineHeight: string
+      },
+      "@media (min-width: 82rem)": {
+        fontFamily: string,
+        fontSize: string,
+        fontWeight: number,
+        letterSpacing: number,
+        lineHeight: string
+      },
+      "@media (min-width: 99rem)": {
+        fontFamily: string,
+        fontSize: string,
+        fontWeight: number,
+        letterSpacing: number,
+        lineHeight: string
+      },
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    },
+    quotation02: {
+      "@media (min-width: 42rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 66rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 82rem)": {
+        fontSize: string,
+        lineHeight: string
+      },
+      "@media (min-width: 99rem)": {
+        fontSize: string
+      },
+      fontFamily: string,
+      fontSize: string,
+      fontWeight: number,
+      letterSpacing: number,
+      lineHeight: string
+    }
+  };
+  declare export function fontFamily(
+    name: string
+  ): {
+    fontFamily: string
+  };
+
+  declare export function fontWeight(
+    weight: string
+  ): {
+    fontWeight: number
+  };
+
+  declare export function getTypeSize(step: number): number;
+
+  declare export function print(block: { [key: string]: any }): string;
+
+  declare var fontFamily: typeof npm$namespace$fontFamily;
+
+  declare var npm$namespace$fontFamily: {
+    prototype: typeof fontFamily$prototype
+  };
+  declare var fontFamily$prototype: {};
+  declare var fontWeight: typeof npm$namespace$fontWeight;
+
+  declare var npm$namespace$fontWeight: {
+    prototype: typeof fontWeight$prototype
+  };
+  declare var fontWeight$prototype: {};
+  declare var getTypeSize: typeof npm$namespace$getTypeSize;
+
+  declare var npm$namespace$getTypeSize: {
+    prototype: typeof getTypeSize$prototype
+  };
+  declare var getTypeSize$prototype: {};
+  declare var print: typeof npm$namespace$print;
+
+  declare var npm$namespace$print: {
+    prototype: typeof print$prototype
+  };
+  declare var print$prototype: {};
+}
