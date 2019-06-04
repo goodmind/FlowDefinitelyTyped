90493c9d51b0a378407ce2c04dce42ca91ec104f
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:54:46 +0500

    [rox-react-native] update types

---
+++ flow-types/types/rox-react-native_vx.x.x/flow_v0.25.x-/rox-react-native.js
@@ -0,0 +1,195 @@
+declare module "rox-react-native" {
+  declare export {};
+
+  declare export interface RoxContainer {
+    [key: string]: Flag | Configuration<> | Variant<>;
+  }
+
+  /**
+   * The register function should be called before the call to Rox.setup()
+   *
+   * https://support.rollout.io/docs/javascript-browser-api#section-register
+   */
+  declare export function register(
+    namespace: string,
+    roxContainer: RoxContainer
+  ): void;
+
+  /**
+   * Initiate connection with Rox servers for the application identified by the application key.
+   * The registered containers will be synced and Rox entities will get the appropriate values.
+   *
+   * https://support.rollout.io/docs/javascript-browser-api#section-setup
+   */
+  declare export function setup(
+    apiKey: string,
+    options?: RoxSetupOptions
+  ): Promise<mixed>;
+
+  declare export interface RoxSetupOptions {
+    version?: string;
+    configurationFetchedHandler?: (fetcherResult: RoxFetcherResult) => void;
+    debugLevel?: "verbose";
+    impressionHandler?: (
+      reporting: RoxReporting,
+      experiment: RoxExperiment,
+      context: mixed
+    ) => void;
+    platform?: string;
+    freeze?: "untilLaunch" | "none";
+  }
+  declare export interface RoxFetcherResult {
+    fetcherStatus:
+      | "APPLIED_FROM_EMBEDDED"
+      | "APPLIED_FROM_CACHE"
+      | "APPLIED_FROM_NETWORK"
+      | "ERROR_FETCH_FAILED";
+    creationDate: Date;
+    hasChanges: boolean;
+    errorDetails?: string;
+  }
+  declare export interface RoxReporting {
+    name: string;
+    value: string;
+  }
+  declare export interface RoxExperiment {
+    identifier: string;
+    name: string;
+    isArchived: boolean;
+    labels: string[];
+  }
+
+  /**
+   * Note that you might have to call unfreeze after setting custom properties such as email after login
+   * https://support.rollout.io/docs/custom-properties
+   */
+  declare export function setCustomNumberProperty(
+    name: string,
+    value: number | (() => number)
+  ): void;
+
+  declare export function setCustomStringProperty(
+    name: string,
+    value: string | (() => string)
+  ): void;
+
+  declare export function setCustomBooleanProperty(
+    name: string,
+    value: boolean | (() => boolean)
+  ): void;
+
+  /**
+   * Unfreeze the state of all flags in code
+   * Calling this function will unfreeze all flags, and using a flag will return it’s most updated value
+   *
+   * https://support.rollout.io/docs/flags-update-flow#section-flag-unfreeze
+   * https://support.rollout.io/docs/javascript-browser-api#section-unfreeze
+   */
+  declare export function unfreeze(namespace?: string): void;
+
+  /**
+   * Pulls the latest configuration and flag values down from the Rollout servers
+   *
+   * https://support.rollout.io/docs/javascript-browser-api#section-fetch
+   */
+  declare export function fetch(): Promise<mixed>;
+
+  /**
+   * Default is untilForeground
+   *
+   * https://support.rollout.io/docs/flags-update-flow#section-flag-freeze-level
+   */
+  declare export type RoxFlagFreezeLevel =
+    | "none"
+    | "untilForeground"
+    | "untilLaunch";
+  declare export interface RoxFlagOptions {
+    freeze?: RoxFlagFreezeLevel;
+  }
+
+  /**
+   * Creates a new Flag
+   * https://support.rollout.io/docs/javascript-browser-api#section--rox-flag-
+   */
+  declare export class Flag {
+    constructor(defaultValue: boolean, options?: RoxFlagOptions): this;
+    name: string;
+    isEnabled(): boolean;
+    getNameDetails(): RoxNameDetails;
+    unfreeze(): void;
+  }
+
+  /**
+   * Used to create and manage Rollout feature flags that determine different predefined values
+   *
+   * https://support.rollout.io/docs/javascript-browser-api#section--rox-variant-
+   */
+  declare export class Variant<T: string = string> {
+    constructor(defaultValue: T, options: T[], name?: string): this;
+    name: string;
+    getValue(): T;
+    getNameDetails(): RoxNameDetails;
+    unfreeze(): void;
+  }
+  declare interface RoxNameDetails {
+    name: string;
+    namespace: string;
+  }
+
+  /**
+   * manages a remote configuration setting with a value of type string, boolean, or number.
+   * The constructor sets the default value for the remote configuration setting
+   *
+   * https://support.rollout.io/docs/javascript-browser-api#section--rox-configuration-
+   */
+  declare export class Configuration<T: string | boolean | number = string> {
+    constructor(defaultValue: T): this;
+    name: string;
+    getValue(): T;
+    unfreeze(): void;
+  }
+
+  /**
+   * Override: Should only be used for development purposes (QA - Feature dev - e2e)
+   *
+   * When you override an existing flag value using the Rox.overrides.setOverride method,
+   * the SDK will disregard existing configuration coming from the dashboard and will
+   * serialize the override on disk this value will be loaded and override the flag
+   * right after you call Rox.setup. To clear the override from the cache you need to
+   * call the Rox.overrides.clearOverride method
+   *
+   * https://support.rollout.io/docs/javascript-browser-api#section--rox-overrides-
+   */
+  declare export interface RoxOverrides {
+    /**
+     * Sets an override value on a specific flag, this function accepts two parameters flag name (
+     * full flag name including namespace) and desired value (from type String).
+     * This function also saves the override value on the local device disk,
+     * so it is "remembered" for the next the SDK is loaded to production.
+     *
+     * https://support.rollout.io/docs/javascript-browser-api#section--rox-overrides-setoverride-
+     *
+     * Note that for boolean flag we still give the value as a string.
+     */
+    setOverride(
+      nameSpacedFlagName: string,
+      value: string | "false" | "true"
+    ): void;
+
+    /**
+     * Clears the override value from the flag (and the disk).
+     *
+     * https://support.rollout.io/docs/javascript-browser-api#section--rox-overrides-clearoverride-
+     */
+    clearOverride(nameSpacedFlagName: string): void;
+    getOriginalValue(nameSpacedFlagName: string): string;
+
+    /**
+     * full flag name including namespace
+     *
+     * https://support.rollout.io/docs/javascript-browser-api#section--rox-overrides-hasoverride-
+     */
+    hasOverride(nameSpacedFlagName: string): boolean;
+  }
+  declare export var overrides: RoxOverrides;
+}
