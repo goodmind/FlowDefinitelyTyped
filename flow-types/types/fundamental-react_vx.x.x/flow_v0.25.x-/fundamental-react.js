fdd4175359625ac688cc10304198eafd417a5503
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:50:35 +0500

    [fundamental-react] update types

---
+++ flow-types/types/fundamental-react_vx.x.x/flow_v0.25.x-/fundamental-react.js
@@ -0,0 +1,115 @@
+declare module "fundamental-react" {
+  declare export { default as ActionBar } from "./lib/ActionBar/ActionBar";
+
+  declare export { default as Alert } from "./lib/Alert/Alert";
+
+  declare export { default as Badge } from "./lib/Badge/Badge";
+
+  declare export { default as Counter } from "./lib/Badge/Counter";
+
+  declare export { default as Label } from "./lib/Badge/Label";
+
+  declare export { default as Status } from "./lib/Badge/Status";
+
+  declare export { default as Breadcrumb } from "./lib/Breadcrumb/Breadcrumb";
+
+  declare export { default as Button } from "./lib/Button/Button";
+
+  declare export { default as ButtonGroup } from "./lib/Button/ButtonGroup";
+
+  declare export { default as Calendar } from "./lib/Calendar/Calendar";
+
+  declare export {
+    default as ComboboxInput
+  } from "./lib/ComboboxInput/ComboboxInput";
+
+  declare export { default as DatePicker } from "./lib/DatePicker/DatePicker";
+
+  declare export { default as Dropdown } from "./lib/Dropdown/Dropdown";
+
+  declare export { default as FormFieldset } from "./lib/Forms/FormFieldset";
+
+  declare export { default as FormInput } from "./lib/Forms/FormInput";
+
+  declare export { default as FormItem } from "./lib/Forms/FormItem";
+
+  declare export { default as FormLabel } from "./lib/Forms/FormLabel";
+
+  declare export { default as FormLegend } from "./lib/Forms/FormLegend";
+
+  declare export { default as FormMessage } from "./lib/Forms/FormMessage";
+
+  declare export {
+    default as FormRadioGroup
+  } from "./lib/Forms/FormRadioGroup";
+
+  declare export { default as FormRadioItem } from "./lib/Forms/FormRadioItem";
+
+  declare export { default as FormSelect } from "./lib/Forms/FormSelect";
+
+  declare export { default as FormSet } from "./lib/Forms/FormSet";
+
+  declare export { default as FormTextarea } from "./lib/Forms/FormTextarea";
+
+  declare export { default as Icon } from "./lib/Icon/Icon";
+
+  declare export { default as Identifier } from "./lib/Identifier/Identifier";
+
+  declare export { default as Image } from "./lib/Image/Image";
+
+  declare export { default as InlineHelp } from "./lib/InlineHelp/InlineHelp";
+
+  declare export { default as FormGroup } from "./lib/InputGroup/FormGroup";
+
+  declare export { default as InputGroup } from "./lib/InputGroup/InputGroup";
+
+  declare export { default as ListGroup } from "./lib/ListGroup/ListGroup";
+
+  declare export {
+    default as LocalizationEditor
+  } from "./lib/LocalizationEditor/LocalizationEditor";
+
+  declare export { default as Menu } from "./lib/Menu/Menu";
+
+  declare export { default as Modal } from "./lib/Modal/Modal";
+
+  declare export { default as MultiInput } from "./lib/MultiInput/MultiInput";
+
+  declare export { default as Pagination } from "./lib/Pagination/Pagination";
+
+  declare export { default as Panel } from "./lib/Panel/Panel";
+
+  declare export { default as PanelGrid } from "./lib/Panel/PanelGrid";
+
+  declare export { default as Popover } from "./lib/Popover/Popover";
+
+  declare export {
+    default as SearchInput
+  } from "./lib/SearchInput/SearchInput";
+
+  declare export { default as Shellbar } from "./lib/Shellbar/Shellbar";
+
+  declare export { default as SideNav } from "./lib/SideNavigation/SideNav";
+
+  declare export { default as Table } from "./lib/Table/Table";
+
+  declare export { default as Tab } from "./lib/Tabs/Tab";
+
+  declare export { default as TabGroup } from "./lib/Tabs/TabGroup";
+
+  declare export { default as ProductTile } from "./lib/Tile/ProductTile";
+
+  declare export { default as Tile } from "./lib/Tile/Tile";
+
+  declare export { default as TileGrid } from "./lib/Tile/TileGrid";
+
+  declare export { default as Time } from "./lib/Time/Time";
+
+  declare export { default as TimePicker } from "./lib/TimePicker/TimePicker";
+
+  declare export { default as Toggle } from "./lib/Toggle/Toggle";
+
+  declare export { default as Token } from "./lib/Token/Token";
+
+  declare export { default as TreeView } from "./lib/TreeView/TreeView";
+}
