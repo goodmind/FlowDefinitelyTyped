f2c50167d0ee959d429955d05e3189d193b75be0
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:52:17 +0500

    [ndn-js] update types

---
+++ flow-types/types/ndn-js_vx.x.x/flow_v0.25.x-/ndn-js.js
@@ -0,0 +1,21 @@
+declare module "ndn-js" {
+  declare export * from "./blob"
+
+  declare export * from "./data"
+
+  declare export * from "./face"
+
+  declare export * from "./interest"
+
+  declare export * from "./key-chain"
+
+  declare export * from "./memory-content-cache"
+
+  declare export * from "./name"
+
+  declare export * from "./network-nack"
+
+  declare export * from "./signature"
+
+  declare export * from "./transport"
+}
