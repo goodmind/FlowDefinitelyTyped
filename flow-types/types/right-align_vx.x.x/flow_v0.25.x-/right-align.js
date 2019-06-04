c4e63c0e919012c17c58f43a107c37d8d79a4208
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:54:39 +0500

    [right-align] update types

---
+++ flow-types/types/right-align_vx.x.x/flow_v0.25.x-/right-align.js
@@ -0,0 +1,7 @@
+declare module "right-align" {
+  declare function right_align(val: string): string;
+
+  declare function right_align(val: string[]): string[];
+
+  declare export default typeof right_align;
+}
