f6316245d161ad2db896a770cb7f0561ef4a7e60
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:47:42 +0500

    [@hapi/cryptiles] update types

---
+++ flow-types/types/@hapi/cryptiles_vx.x.x/flow_v0.25.x-/cryptiles.js
@@ -0,0 +1,18 @@
+declare module "@hapi/cryptiles" {
+  /**
+   * Returns a cryptographically strong pseudo-random data string. Takes a size argument for the length of the string.
+   */
+  declare export function randomString(size: number): string;
+
+  /**
+   * Returns a cryptographically strong pseudo-random data string consisting of only numerical digits (0-9).
+   * Takes a size argument for the length of the string.
+   */
+  declare export function randomDigits(size: number): string;
+
+  /**
+   * Compare two strings using fixed time algorithm (to prevent time-based analysis of MAC digest match).
+   * Returns true if the strings match, false if they differ.
+   */
+  declare export function fixedTimeComparison(a: string, b: string): boolean;
+}
