2ade81aaf1988c992aa70beda975ef2b77149d89
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:48:16 +0500

    [@storybook/preact] update types

---
+++ flow-types/types/@storybook/preact_vx.x.x/flow_v0.25.x-/preact.js
@@ -0,0 +1,54 @@
+declare module "@storybook/preact" {
+  import typeof * as Preact from "preact";
+
+  declare export type Renderable = Preact.AnyComponent | React$Node;
+  declare export type RenderFunction = () => Renderable | Renderable[];
+  declare export interface DecoratorParameters {
+    [key: string]: any;
+  }
+  declare export type StoryDecorator = (
+    story: RenderFunction,
+    context: {
+      kind: string,
+      story: string
+    }
+  ) => Renderable | null;
+  declare export interface Story {
+    +kind: string;
+    add(
+      storyName: string,
+      callback: RenderFunction,
+      parameters?: DecoratorParameters
+    ): this;
+    addDecorator(decorator: StoryDecorator): this;
+    addParameters(parameters: DecoratorParameters): this;
+  }
+  declare export function addDecorator(decorator: StoryDecorator): void;
+
+  declare export function addParameters(parameters: DecoratorParameters): void;
+
+  declare export function clearDecorators(): void;
+
+  declare export function configure(fn: () => void, module: NodeModule): void;
+
+  declare export function setAddon(addon: { [key: string]: any }): void;
+
+  declare export function storiesOf(name: string, module: NodeModule): Story;
+
+  declare export function storiesOf<T>(
+    name: string,
+    module: NodeModule
+  ): Story & T;
+
+  declare export function forceReRender(): void;
+
+  declare export interface StoryObject {
+    name: string;
+    render: RenderFunction;
+  }
+  declare export interface StoryBucket {
+    kind: string;
+    stories: StoryObject[];
+  }
+  declare export function getStorybook(): StoryBucket[];
+}
