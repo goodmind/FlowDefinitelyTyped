a01c1705e81cb6cd0ae9687f0f5bb1969b26d01f
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:55:56 +0500

    [viewport-list] update types

---
+++ flow-types/types/viewport-list_vx.x.x/flow_v0.25.x-/viewport-list.js
@@ -0,0 +1,18 @@
+declare module "viewport-list" {
+  declare export default typeof viewportList;
+
+  declare interface Device {
+    name: string;
+    platform: string;
+    os: string;
+    size: string;
+    release: string;
+  }
+
+  /**
+   * Returns a list of devices and their veiwports
+   * @param items - An array of device names to fetch
+   * @returns - An array of viewports
+   */
+  declare function viewportList(items?: string[]): Device[];
+}
