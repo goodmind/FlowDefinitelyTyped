a5e3dbb4a60c14e20636b92f82e2b4d18068d8f6
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:53:20 +0500

    [quick-hash] update types

---
+++ flow-types/types/quick-hash_vx.x.x/flow_v0.25.x-/quick-hash.js
@@ -0,0 +1,11 @@
+declare module "quick-hash" {
+  /**
+   * Murmur hash optimized for performance, not collision avoidance.
+   * @param key the string to hash
+   * @param seed a seed for hashing
+   * @returns A string of 5 to 7 alpha-numeric characters
+   */
+  declare function quickHash(key: string, seed?: number): string;
+
+  declare export default typeof quickHash;
+}
