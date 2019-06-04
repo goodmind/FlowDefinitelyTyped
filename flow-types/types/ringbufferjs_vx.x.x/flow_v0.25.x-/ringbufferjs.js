85b029ba2afdb2837580b292f68113c3a378533a
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:54:41 +0500

    [ringbufferjs] update types

---
+++ flow-types/types/ringbufferjs_vx.x.x/flow_v0.25.x-/ringbufferjs.js
@@ -0,0 +1,21 @@
+declare module "ringbufferjs" {
+  declare export default typeof RingBuffer;
+
+  declare class RingBuffer<T> {
+    _elements: T[];
+    _first: number;
+    _last: number;
+    _size: number;
+    _evictedCb: (element: T) => any;
+    constructor(capacity: number, evictedCB?: (element: T) => any): this;
+    capacity(): number;
+    isEmpty(): boolean;
+    isFull(): boolean;
+    peek(): T;
+    peekN(count: number): T[];
+    deq(): T;
+    deqN(count: number): T[];
+    enq(element: T): number;
+    size(): number;
+  }
+}
