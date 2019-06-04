e75cb4a516c069bcc22e09b924d39acc3a74dbf6
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:52:41 +0500

    [parse-color] update types

---
+++ flow-types/types/parse-color_vx.x.x/flow_v0.25.x-/parse-color.js
@@ -0,0 +1,17 @@
+declare module "parse-color" {
+  declare interface parseColor$Color {
+    rgb: [number, number, number];
+    hsl: [number, number, number];
+    hsv: [number, number, number];
+    cmyk: [number, number, number, number];
+    keyword: string;
+    hex: string;
+    rgba: [number, number, number, number];
+    hsla: [number, number, number, number];
+    hsva: [number, number, number, number];
+    cmyka: [number, number, number, number, number];
+  }
+  declare function parseColor(cstr: string): parseColor$Color;
+
+  declare export default typeof parseColor;
+}
