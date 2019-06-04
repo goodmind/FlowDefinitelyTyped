df5fe37d25290d8a87b17e38d13ffe29c256fdd4
andretshurotshka <andwebar@gmail.com>
2019-06-04 12:54:15 +0500

    [react-tabs-redux] update types

---
+++ flow-types/types/react-tabs-redux_vx.x.x/flow_v0.25.x-/react-tabs-redux.js
@@ -0,0 +1,7 @@
+declare module "react-tabs-redux" {
+  declare export { Tabs, TabsProps } from "./Tabs";
+
+  declare export { TabLink, TabLinkProps } from "./TabLink";
+
+  declare export { TabContent, TabContentProps } from "./TabContent";
+}
